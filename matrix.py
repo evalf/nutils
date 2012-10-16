d9e531cc46bcc153ac20c7c4c7bc3a8bf88f3873
gertjan <gertjan@6d9cea27-b75e-416a-b27a-0fd965b36497>
2012-10-16 17:10:08 +0000

    polished matrix object added Stress func to bone

--- matrix.py
+++ matrix.py
@@ -1,5 +1,152 @@
 from . import util, numpy, _
 from scipy.sparse.sparsetools.csr import _csr
+from scipy.sparse.linalg.isolve import _iterative
+
+def cg( matvec, b, x0=None, tol=1e-5, maxiter=None, precon=None, title='solving system' ):
+  'CG solver'
+
+  n = len(b)
+
+  if title:
+    progress = util.progressbar( n=numpy.log(tol), title='%s [CG:%d]' % (title,n) )
+    clock = util.Clock( .1 )
+  else:
+    clock = False
+
+  if x0 is None:
+    x = numpy.zeros( n )
+  else:
+    assert len(x0) == n
+    x = x0
+
+  if maxiter is None:
+    maxiter = n*10
+
+  assert x.dtype == float
+  revcom = _iterative.dcgrevcom
+  stoptest = _iterative.dstoptest2
+
+  resid = tol
+  ndx1 = 1
+  ndx2 = -1
+  work = numpy.zeros(4*n)
+  ijob = 1
+  info = 0
+  ftflag = True
+  bnrm2 = -1.0
+  iter_ = maxiter
+  while True:
+    x, iter_, resid, info, ndx1, ndx2, sclr1, sclr2, ijob = revcom( b, x, work, iter_, resid, info, ndx1, ndx2, ijob )
+    if clock:
+      progress.update( numpy.log( numpy.linalg.norm( rhs - matrix * vec ) ) )
+    slice1 = slice(ndx1-1, ndx1-1+n)
+    slice2 = slice(ndx2-1, ndx2-1+n)
+    if ijob == 1:
+      work[slice2] *= sclr2
+      work[slice2] += sclr1 * matvec(work[slice1])
+    elif ijob == 2:
+      work[slice1] = precon(work[slice2]) if precon else work[slice2]
+    elif ijob == 3:
+      work[slice2] *= sclr2
+      work[slice2] += sclr1 * matvec(x)
+    elif ijob == 4:
+      if ftflag:
+        info = -1
+        ftflag = False
+      bnrm2, resid, info = stoptest(work[slice1], b, bnrm2, tol, info)
+    else:
+      assert ijob == -1
+      break
+    ijob = 2
+
+  if info > 0 and iter_ == maxiter and resid > tol:
+    #info isn't set appropriately otherwise
+    info = iter_
+
+  if title:
+    progress.finish()
+
+  assert info == 0
+  return x
+
+def gmres( matvec, b, x0=None, tol=1e-5, restrt=None, maxiter=None, precon=None, title='solving system' ):
+  'GMRES solver'
+
+  n = len(b)
+
+  if title:
+    progress = util.progressbar( n=numpy.log(tol), title='%s [GMRES:%d]' % (title,n) )
+    clock = util.Clock( .1 )
+  else:
+    clock = False
+
+  if x0 is None:
+    x = numpy.zeros( n )
+  else:
+    assert len(x0) == n
+    x = x0
+
+  if maxiter is None:
+    maxiter = n*10
+
+  if restrt is None:
+    restrt = 20
+  restrt = min(restrt, n)
+
+  assert x.dtype == float
+  revcom = _iterative.dgmresrevcom
+  stoptest = _iterative.dstoptest2
+
+  resid = tol
+  ndx1 = 1
+  ndx2 = -1
+  work  = numpy.zeros((6+restrt)*n,dtype=x.dtype)
+  work2 = numpy.zeros((restrt+1)*(2*restrt+2),dtype=x.dtype)
+  ijob = 1
+  info = 0
+  ftflag = True
+  bnrm2 = -1.0
+  iter_ = maxiter
+  old_ijob = ijob
+  first_pass = True
+  resid_ready = False
+  while True:
+    x, iter_, resid, info, ndx1, ndx2, sclr1, sclr2, ijob = revcom(b, x, restrt, work, work2, iter_, resid, info, ndx1, ndx2, ijob)
+    slice1 = slice(ndx1-1, ndx1-1+n)
+    slice2 = slice(ndx2-1, ndx2-1+n)
+    if (ijob == -1): # gmres success, update last residual
+      break
+    elif (ijob == 1):
+      work[slice2] *= sclr2
+      work[slice2] += sclr1*matvec(x)
+    elif (ijob == 2):
+      work[slice1] = precon(work[slice2]) if precon else work[slice2]
+      if not first_pass and old_ijob==3:
+        resid_ready = True
+      first_pass = False
+    elif (ijob == 3):
+      work[slice2] *= sclr2
+      work[slice2] += sclr1*matvec(work[slice1])
+      if resid_ready and clock:
+        progress.update( numpy.log(resid) )
+        resid_ready = False
+    elif (ijob == 4):
+      if ftflag:
+        info = -1
+        ftflag = False
+      bnrm2, resid, info = stoptest(work[slice1], b, bnrm2, tol, info)
+    old_ijob = ijob
+    ijob = 2
+
+  if info >= 0 and resid > tol:
+    #info isn't set appropriately otherwise
+    info = maxiter
+
+  if title:
+    progress.finish()
+
+  assert info == 0
+  return x
 
 def parsecons( constrain, lconstrain, rconstrain, shape ):
   'parse constraints'
@@ -20,49 +167,33 @@ def parsecons( constrain, lconstrain, rconstrain, shape ):
   x[I] = 0
   return x, I, J
 
-def iterative_solve( matmul, rhs, title=True, symmetric=False, tol=0, maxiter=999999 ):
-  'solve linear system iteratively'
+class Matrix( object ):
+  'matrix base class'
 
-  from scipy.sparse import linalg
+  def __init__( self, (nrows,ncols) ):
+    'constructor'
 
-  if title is True:
-    title = 'solve system'
+    self.shape = nrows, ncols
+    self.size = nrows * ncols
 
-  shape = rhs.shape * 2
-  matrix = linalg.LinearOperator( matvec=matmul, shape=shape, dtype=float )
+class DenseSubMatrix( Matrix ):
+  'dense but non-contiguous data'
 
-  progress = None
-  callback = None
+  def __init__( self, data, indices ):
+    'constructor'
 
-  if title:
-    progress = util.progressbar( n=numpy.log(tol), title='%s [%s:%d]' % ( title, symmetric and 'CG' or 'GMRES', matrix.shape[0] ) )
-    clock = util.Clock( .1 )
-    callback = lambda vec: clock and progress.update(
-      numpy.log( vec if not symmetric else numpy.linalg.norm( rhs - matrix * vec ) ) )
-
-  solver = linalg.cg if symmetric else linalg.gmres
-  lhs, status = solver( matrix, rhs, callback=callback, tol=tol, maxiter=maxiter )
-  assert status == 0, 'solution failed to converge (status=%d)' % status
-
-  if progress:
-    progress.finish()
-
-  return lhs
-
-class SparseBlock( object ):
-  'sparse data block that supports in-place addition'
-
-  def __init__( self, data, index ):
     self.data = data
-    self.index = index
+    self.indices = indices
+    Matrix.__init__( self, indices.shape )
 
-  def __iadd__( self, data ):
-    self.data[ self.index ] += data
+  def __iadd__( self, other ):
+    'in place addition'
 
-  def __str__( self ):
-    return str( self.data[ self.index ] )
+    assert self.shape == other.shape
+    self.data[ self.indices ] += other
+    return self
 
-class SparseMatrix( object ):
+class SparseMatrix( Matrix ):
   'matrix wrapper class'
 
   def __init__( self, graph, ncols=None ):
@@ -81,8 +212,7 @@ class SparseMatrix( object ):
       for irow, icols in enumerate( graph ):
         a, b = self.indptr[irow:irow+2]
         self.indices[a:b] = icols
-
-    self.shape = nrows, ncols or nrows
+    Matrix.__init__( self, (nrows, ncols or nrows) )
 
   def reshape( self, (nrows,ncols) ):
     'reshape matrix'
@@ -123,74 +253,67 @@ class SparseMatrix( object ):
     return result
 
   def __getitem__( self, (rows,cols) ):
-    'isolate block (for addition)'
+    'get submatrix'
 
-    import scipy.sparse
-
-    J = numpy.empty( [len(rows),len(cols)], dtype=int )
-    if cols.dtype == bool:
+    if isinstance(cols,numpy.ndarray) and cols.dtype == bool:
       assert len(cols) == self.shape[1]
       select = cols
       ncols = select.sum()
-    else:
+    elif isinstance(cols,numpy.ndarray) and cols.dtype == int:
       ncols = cols.size
       select = numpy.zeros( self.shape[1], dtype=bool )
       select[cols] = True
+    else:
+      raise Excaption, 'invalid column argument'
 
-    if rows.dtype == bool:
+    if isinstance(rows,numpy.ndarray) and rows.dtype == bool:
       rows, = rows.nonzero()
-    nrows = rows.size
+      nrows = len(rows)
+    elif isinstance(rows,numpy.ndarray) and rows.dtype == int:
+      nrows = len(rows)
+    else:
+      raise Excaption, 'invalid row argument'
 
-    indptr = numpy.empty( len(rows)+1, dtype=int )
-    maxentries = numpy.minimum( self.indptr[rows+1] - self.indptr[rows], ncols ).sum()
-    indices = numpy.empty( maxentries, dtype=int ) # allocate for worst case
-
+    indptr = numpy.empty( nrows+1, dtype=int )
+    I = numpy.empty( numpy.minimum( self.indptr[rows+1] - self.indptr[rows], ncols ).sum(), dtype=int ) # allocate for worst case
     indptr[0] = 0
     for n, irow in enumerate( rows ):
       a, b = self.indptr[irow:irow+2]
       where, = select[ self.indices[a:b] ].nonzero()
       c = indptr[n]
       d = c + where.size
-      indices[c:d] = a + where
+      I[c:d] = a + where
       indptr[n+1] = d
 
-    # TODO return proper CSR matrix
-    # matrix = scipy.sparse.csr_matrix( (self.matrix.data,indices,indptr), shape=(nrows,ncols) )
-
-    assert indptr[nrows] == nrows * ncols
-    return SparseBlock( self.data, indices.reshape(nrows,ncols) )
+    return DenseSubMatrix( self.data, I.reshape(nrows,ncols) ) if d == nrows * ncols \
+      else SparseMatrix( (self.data[I],self.indices[I],indptr), ncols )
 
   def __iadd__( self, other ):
     'in place addition'
 
-    assert isinstance( other, self.__class__ )
-    assert self.shape == other.shape
+    assert isinstance( other, self.__class__ ) and self.shape == other.shape
     if numpy.all( self.indptr == other.indptr ) and numpy.all( self.indices == other.indices ):
       self.data += other.data
-      return self
-    for irow in range( self.shape[0] ):
-      c, d = other.indptr[irow:irow+2]
-      if c == d:
-        continue
-      a, b = self.indptr[irow:irow+2]
-      I = self.indices[a:b]
-      J = other.indices[c:d]
-      n = a + I.searchsorted( J )
-      assert numpy.all( self.indices[n] == J )
-      self.data[n] += other.data[c:d]
+    else:
+      _csr.csr_plus_csr( self.shape[0], self.shape[1], self.indptr,  self.indices,  self.data,
+                                                      other.indptr, other.indices, other.data,
+                                                       self.indptr,  self.indices,  self.data )
     return self
 
   def __setitem__( self, item, value ):
-    'sucks'
+    'set submatrix'
 
-    pass
+    assert self.data is value.data # apparently we are assigning ourselves
 
   @property
   def T( self ):
     'transpose'
 
-    raise NotImplementedError
-    return SparseMatrix( self.matrix.T.tocsr() )
+    data = numpy.empty_like( self.data )
+    indices = numpy.empty_like( self.indices )
+    indptr = numpy.empty_like( self.indptr )
+    _csr.csr_tocsc( self.shape[0], self.shape[1], self.indptr, self.indices, self.data, indptr, indices, data )
+    return SparseMatrix( (data,indices,indptr), self.shape[0] )
 
   def toarray( self ):
     'convert to numpy array'
@@ -215,11 +338,11 @@ class SparseMatrix( object ):
       supp[irow] = a == b or tol != 0 and numpy.all( numpy.abs( self.data[a:b] ) < tol )
     return supp
 
-  def solve( self, b=0, constrain=None, lconstrain=None, rconstrain=None, tol=0, **kwargs ):
+  def solve( self, b=0, constrain=None, lconstrain=None, rconstrain=None, tol=0, symmetric=False, maxiter=99999 ):
     'solve'
 
     if tol == 0:
-      return self.todense().solve( b=b, constrain=constrain, lconstrain=lconstrain, rconstrain=rconstrain, tol=tol, **kwargs )
+      return self.todense().solve( b=b, constrain=constrain, lconstrain=lconstrain, rconstrain=rconstrain )
   
     b = numpy.asarray( b, dtype=float )
     if b.ndim == 0:
@@ -230,8 +353,10 @@ class SparseMatrix( object ):
         b.resize( self.shape[0] )
       assert b.shape == self.shape[:1]
   
+    solver = cg if symmetric else gmres
+
     if constrain is lconstrain is rconstrain is None:
-      return iterative_solve( self.matvec, b, tol=tol, **kwargs )
+      return solver( self.matvec, b, tol=tol, maxiter=maxiter )
 
     x, I, J = parsecons( constrain, lconstrain, rconstrain, self.shape )
     b = ( b - self.matvec(x) )[I]
@@ -239,10 +364,10 @@ class SparseMatrix( object ):
     def matvec( v ):
       tmpvec[J] = v
       return self.matvec(tmpvec)[I]
-    x[J] = iterative_solve( matvec, b, tol=tol, **kwargs )
+    x[J] = solver( matvec, b, tol=tol, maxiter=maxiter )
     return x
 
-class DenseMatrix( object ):
+class DenseMatrix( Matrix ):
   'matrix wrapper class'
 
   def __init__( self, shape ):
@@ -250,14 +375,11 @@ class DenseMatrix( object ):
 
     if isinstance( shape, numpy.ndarray ):
       self.data = shape
-      self.shape = self.data.shape
     else:
       if isinstance( shape, int ):
-        self.shape = shape, shape
-      else:
-        assert len(shape) == 2
-        self.shape = tuple(shape)
-      self.data = numpy.zeros( self.shape )
+        shape = shape, shape
+      self.data = numpy.zeros( shape )
+    Matrix.__init__( self, self.data.shape )
 
   def addblock( self, rows, cols, vals ):
     'add matrix data'
@@ -281,6 +403,7 @@ class DenseMatrix( object ):
     return numpy.dot( self.data, other )
 
   def __getitem__( self, rows, cols ):
+    'get submatrix'
 
     return self.data[ rows[:,_], cols[:,_] ]
 
