834c5e4c966fb02781865c99d8c80ad4e6495fb8
gertjan <gertjan@6d9cea27-b75e-416a-b27a-0fd965b36497>
2013-05-29 18:31:58 +0000

    Hooke law update in library

--- library.py
+++ library.py
@@ -1,20 +1,45 @@
-from finity import util, function, _
+from . import function, _
+import numpy
 
-@util.withrepr
-def Hooke( **kwargs ):
+def _tryall( obj, prefix, kwargs ):
+  for name in dir( obj ):
+    if name.startswith( prefix ):
+      try:
+        return getattr( obj, name )( **kwargs )
+      except TypeError:
+        pass
+  raise Exception, 'not supported: ' + ', '.join( kwargs.keys() )
 
-  if len(kwargs)!=2:
-    raise ValueError( 'Exactly two arguments should be provided' )
+class Hooke:
 
-  if 'lmbda' in kwargs and 'mu' in kwargs:
-    lmbda = kwargs['lmbda']
-    mu    = kwargs['mu']
-  elif 'E' in kwargs and 'nu' in kwargs:
-    lmbda = kwargs['E']*kwargs['nu']/((1.+kwargs['nu'])*(1.-2.*kwargs['nu']))
-    mu    = kwargs['E']/(2.*(1.+kwargs['nu']))
-  else:
-    raise ValueError('Illegal argument combination. Valid combinations are: (lmbda,mu), (E,nu)')
+  def __init__( self, **kwargs ):
+    
+    if len(kwargs)!=2:
+      raise ValueError( 'exactly two arguments expected, found %d' % len(kwargs) )
 
-  return lambda disp, coords: \
-    lmbda * disp.div(coords)[...,_,_] * function.eye( coords.shape[0] ) \
-      + (2*mu) * disp.symgrad(coords)
+    _tryall( self, '_set_from_', kwargs )
+    for key, value in kwargs.items():
+      numpy.testing.assert_almost_equal( value, getattr(self,key) )
+
+  def _set_from_lame( self, lmbda, mu ):
+    self.lmbda = lmbda
+    self.mu = mu
+
+  def _set_from_poisson_young( self, nu, E ):
+    self.lmbda = (nu*E)/((1.+nu)*(1.-2.*nu))
+    self.mu = E/(2.*(1.+nu))
+
+  def __call__ ( self, disp, coords ): 
+    return self.lmbda * disp.div(coords)[...,_,_] * function.eye( coords.shape[0] ) \
+      + (2*self.mu) * disp.symgrad(coords)
+    
+  def __str__( self ):
+    return 'Hooke(%s,%s)' % ( self.mu, self.lmbda )
+
+  @property
+  def E( self ):
+    return self.mu * (3.*self.lmbda+2.*self.mu) / (self.lmbda+self.mu)
+
+  @property
+  def nu( self ):
+    return self.lmbda / (2.*(self.lmbda+self.mu))
