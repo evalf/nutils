3883b6dead58f6d3a83b756574a14400e53e5128
Gertjan van Zwieten <git@gjvz.nl>
2014-02-07 21:54:02 +0100

    add makefile

---
+++ Makefile
@@ -0,0 +1,14 @@
+all: numeric test
+
+numeric:
+	$(MAKE) -C nutils/numeric test_c
+
+test:
+	python tests/test.py
+
+clean:
+	$(MAKE) -C nutils/numeric clean
+
+.PHONY: all numeric test clean
+
+# vim:noexpandtab
--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 import os, sys, traceback
 
-count = [ 0, 0, 0 ] # ok, failed, error
+status = [ 0, 0, 0 ] # ok, failed, error
 
 def unittest( func ):
   try:
@@ -8,18 +8,18 @@ def unittest( func ):
   except KeyboardInterrupt:
     raise
   except AssertionError, e:
-    count[1] += 1
+    status[1] += 1
     sys.stdout.write( 'F' )
     print
-    print '##', func.func_name, 'FAILED(%d):' % count[1], e
+    print '##', func.func_name, 'FAILED(%d):' % status[1], e
   except:
-    count[2] += 1
+    status[2] += 1
     sys.stdout.write( 'E' )
     print
-    print '##', func.func_name, 'ERROR(%d)' % count[2]
+    print '##', func.func_name, 'ERROR(%d)' % status[2]
     traceback.print_exc()
   else:
-    count[0] += 1
+    status[0] += 1
     sys.stdout.write( '.' )
     sys.stdout.flush()
   return func
@@ -34,5 +34,5 @@ if __name__ == '__main__':
       print '%d.' % count, name.upper(), '|',
       __import__( name )
       print
-  print '%d tests successful, %d failed, %d errors' % tuple( count )
-  raise SystemExit( count[1] + count[2] )
+  print '%d tests successful, %d failed, %d errors' % tuple( status )
+  raise SystemExit( status[1] + status[2] )
