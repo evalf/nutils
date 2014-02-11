9341408b068a365e30f8c8d8907aa4f1dddf64d3
Gertjan van Zwieten <git@gjvz.nl>
2014-02-11 18:36:40 +0100

    reactivate pointset cache

--- nutils/pointset.py
+++ nutils/pointset.py
@@ -8,13 +8,11 @@ class DynamicPointset( object ):
     self.args = args
 
   def __getitem__( self, elem ):
-    return elem.pointset( self.name, *self.args )
-    reference = elem.reference
     try:
-      return self.cache[ reference ]
+      return self.cache[ elem ]
     except:
-      pointset = reference.pointset( self.name, *self.args )
-      self.cache[ reference ] = pointset
+      pointset = elem.pointset( self.name, *self.args )
+      self.cache[ elem ] = pointset
       return pointset
 
 class Gauss( DynamicPointset ):
@@ -32,7 +30,7 @@ class StaticPointset( object ):
     self.refmap = refmap
 
   def __getitem__( self, elem ):
-    return self.refmap[ elem.reference ]
+    return self.refmap[ elem ]
 
 
 _lin = element.Simplex(1)
