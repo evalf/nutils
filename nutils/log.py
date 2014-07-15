ad2eb77d691168d3af8c70e8062f0af3744a64f6
Gertjan van Zwieten <git@gjvz.nl>
2014-07-15 20:28:18 +0200

    fix nested logging

--- nutils/log.py
+++ nutils/log.py
@@ -118,7 +118,20 @@ colorpicker = {
 
 class Log( object ):
 
-  pass
+  def kill( self ):
+    raise Exception, 'primary loggers are immortable'
+
+  @property
+  def living( self ):
+    return self
+
+  @property
+  def alive( self ):
+    return True
+
+  @property
+  def parent( self ):
+    raise Exception, 'primary loggers have no parents'
 
 
 class StdLog( Log ):
@@ -140,93 +153,136 @@ class HtmlLog( Log ):
   'html log'
 
   def __init__( self, html ):
-    self.html = html
+    Log.__init__( self )
+    self.__html = html
 
   def getstream( self, level, *contexts ):
-    return HtmlStream( level, contexts, self.html )
-
-
-class ContextLog( Log ):
-  'static text with parent'
-
-  def __init__( self, title, parent=None ):
-    self.title = title
-    self.parent = parent or _getlog()
-
-  def getstream( self, level, *contexts ):
-    return self.parent.getstream( level, self.title, *contexts )
-
-
-class IterLog( Log ):
-  'iterable context logger that updates progress info'
-
-  def __init__( self, title, iterator, length=None, parent=None ):
-    self.title = title
-    self.parent = parent or _getlog()
-    self.length = length
-    self.iterator = iterator
-    self.index = -1
-
-    # clock
-    self.dt = core.getprop( 'progress_interval', 1. )
-    self.dtexp = core.getprop( 'progress_interval_scale', 2 )
-    self.dtmax = core.getprop( 'progress_interval_max', 0 )
-    self.tnext = time.time() + self.dt
-
-  def mktitle( self ):
-    self.tnext = time.time() + self.dt
-    return '%s %d' % ( self.title, self.index ) if self.length is None \
-      else '%s %d/%d (%d%%)' % ( self.title, self.index, self.length, (self.index-.5) * 100. / self.length )
-
-  def __iter__( self ):
-    self.index = 0
-    return self
-
-  def next( self ):
-    if time.time() > self.tnext:
-      if self.dtexp != 1:
-        self.dt *= self.dtexp
-        if self.dt > self.dtmax > 0:
-          self.dt = self.dtmax
-      self.parent.getstream( 'progress' ).write( self.mktitle() + '\n' )
-    self.index += 1
-    try:
-      return self.iterator.next()
-    except:
-      self.index = -1
-      raise
-
-  def getstream( self, level, *contexts ):
-    return self.parent.getstream( level, self.mktitle(), *contexts ) if self.index >= 0 \
-      else self.parent.getstream( level, *contexts )
+    return HtmlStream( level, contexts, self.__html )
 
 
 class CaptureLog( Log ):
   'capture output without printing'
 
   def __init__( self ):
-    self.buf = ''
+    Log.__init__( self )
+    self.__buf = ''
 
   def __nonzero__( self ):
-    return bool( self.buf )
+    return bool( self.__buf )
 
   def __str__( self ):
-    return self.buf
+    return self.__buf
 
   def getstream( self, level, *contexts ):
     for context in contexts:
-      self.buf += '%s > ' % context
+      self.__buf += '%s > ' % context
     return self
 
   def write( self, text ):
-    self.buf += text
+    self.__buf += text
+
+
+class ContextLog( Log ):
+
+  def __init__( self ):
+    self.__parent = _getlog()
+    self.__alive = True
+    self.__awake = True
+
+  @property
+  def living( self ):
+    return self if self.__alive else self.parent
+
+  def kill( self ):
+    self.__alive = False
+
+  def sleep( self ):
+    self.__awake = False
+
+  def wake( self ):
+    self.__awake = True
+
+  @property
+  def alive( self ):
+    return self.__alive
+
+  @property
+  def awake( self ):
+    return self.__alive
+
+  @property
+  def parent( self ):
+    if not self.__parent.alive:
+      self.__parent = self.__parent.parent
+    return self.__parent
+
+  def getstream( self, level, *contexts ):
+    assert self.__alive, 'logging from a dead object'
+    return self.parent.getstream( level, self.getcontext(), *contexts ) if self.awake \
+      else self.parent.getstream( level, *contexts )
+
+  def getcontext( self ):
+    return '<no context>'
+
+
+class StaticLog( ContextLog ):
+  'static text with parent'
+
+  def __init__( self, context ):
+    ContextLog.__init__( self )
+    self.__context = context
+
+  def getcontext( self ):
+    return self.__context
+
+
+class IterLog( ContextLog ):
+  'iterable context logger that updates progress info'
+
+  def __init__( self, context, iterator, length=None ):
+    ContextLog.__init__( self )
+
+    self.__context = context
+    self.__length = length
+    self.__iterator = iterator
+    self.__index = 0
+
+    # clock
+    self.__dt = core.getprop( 'progress_interval', 1. )
+    self.__dtexp = core.getprop( 'progress_interval_scale', 2 )
+    self.__dtmax = core.getprop( 'progress_interval_max', 0 )
+    self.__tnext = time.time() + self.__dt
+
+    self.sleep()
+
+  def getcontext( self ):
+    self.__tnext = time.time() + self.__dt
+    return '%s %d' % ( self.__context, self.__index ) if self.__length is None \
+      else '%s %d/%d (%d%%)' % ( self.__context, self.__index, self.__length, (self.__index-.5) * 100. / self.__length )
+
+  def __iter__( self ):
+    try:
+      self.wake()
+      for item in self.__iterator:
+        self.__index += 1
+        if time.time() > self.__tnext:
+          if self.__dtexp != 1:
+            self.__dt *= self.__dtexp
+            if self.__dt > self.__dtmax > 0:
+              self.__dt = self.__dtmax
+          self.parent.getstream( 'progress' ).write( self.getcontext() + '\n' )
+        yield item
+    finally:
+      self.kill()
 
 
 # instances
+
 stdlog = StdLog()
 
 # helper functions
-_getlog = lambda: core.getprop( 'log', stdlog )
+
+_getlog = lambda: core.getprop( 'log', stdlog ).living
 _getstream = lambda level: _getlog().getstream( level )
 _mklog = lambda level: lambda *args, **kw: print( *args, file=_getstream(level), **kw )
 
@@ -240,15 +296,20 @@ def range( title, *args ):
   items = _range( *args )
   return IterLog( title, _iter(items), len(items) )
 
-def iter( title, iterable, length=None, parent=None ):
-  return IterLog( title, _iter(iterable), len(iterable) if length is None else length, parent )
+def iter( title, iterable, length=None ):
+  if length is None:
+    try:
+      length = len(iterable)
+    except:
+      pass
+  return IterLog( title, _iter(iterable), length )
 
-def enumerate( title, iterable, length=None, parent=None ):
-  return IterLog( title, _enumerate(iterable), len(iterable) if length is None else length, parent )
+def enumerate( title, iterable, length=None ):
+  return IterLog( title, _enumerate(iterable), len(iterable) if length is None else length )
 
-def count( title, start=0, parent=None ):
+def count( title, start=0 ):
   from itertools import count
-  return IterLog( title, count(start), None, parent )
+  return IterLog( title, count(start), None )
     
 def stack( msg ):
   'print stack trace'
@@ -264,7 +325,7 @@ def stack( msg ):
 
 def title( f ): # decorator
   def wrapped( *args, **kwargs ):
-    __log__ = ContextLog( kwargs.pop( 'title', f.func_name ) )
+    __log__ = StaticLog( kwargs.pop( 'title', f.func_name ) )
     return f( *args, **kwargs )
   return wrapped
 
