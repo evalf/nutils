443cc661599529bac74f9d7dbd60efc713b308e7
Gertjan van Zwieten <gertjanvanzwieten@gmail.com>
2014-12-17 16:17:28 +0100

    add prism ischeme unit tests

--- tests/test_ischeme.py
+++ tests/test_ischeme.py
@@ -10,6 +10,7 @@ class TestIscheme( object ):
     self.tetrahedron = element.SimplexReference(3)
     self.quad = element.SimplexReference(1)**2
     self.hexagon = element.SimplexReference(1)**3
+    self.prism = element.SimplexReference(1)*element.SimplexReference(2)
 
   def _test( self, ref, ptype, target_points, target_weights=None ):
     points, weights = ref.getischeme( ptype )
@@ -32,26 +33,29 @@ class TestIscheme( object ):
     self._test( self.line, 'gauss4', [[.5-.5*b],[.5],[.5+.5*b]], [5/18.,4/9.,5/18.] )
     self._test( self.line, 'gauss5', [[.5-.5*b],[.5],[.5+.5*b]], [5/18.,4/9.,5/18.] )
     self._test( self.quad, 'gauss1', [[.5,.5]], [1.] )
-    self._test( self.quad, 'gauss2', [[.5-.5*a,.5-.5*a],[.5-.5*a,.5+.5*a],[.5+.5*a,.5-.5*a],[.5+.5*a,.5+.5*a]], [.25,.25,.25,.25] )
+    self._test( self.quad, 'gauss2', [[.5-.5*a,.5-.5*a],[.5-.5*a,.5+.5*a],[.5+.5*a,.5-.5*a],[.5+.5*a,.5+.5*a]], [1/4.]*4 )
     self._test( self.quad, 'gauss1,4', [[.5,.5-.5*b],[.5,.5],[.5,.5+.5*b]], [5/18.,4/9.,5/18.] )
     self._test( self.triangle, 'gauss1', [[1/3.,1/3.]], [.5] )
-    self._test( self.triangle, 'gauss2', [[2/3.,1/6.],[1/6.,2/3.],[1/6.,1/6.]], [1/6.,1/6.,1/6.] )
+    self._test( self.triangle, 'gauss2', [[2/3.,1/6.],[1/6.,2/3.],[1/6.,1/6.]], [1/6.]*3 )
     self._test( self.triangle, 'gauss3', [[1/3.,1/3.],[3/5.,1/5.],[1/5.,3/5.],[1/5.,1/5.]], [-9/32.,25/96.,25/96.,25/96.] )
 
   def testUniform( self ):
     self._test( self.line, 'uniform1', [[.5]], [1.] )
     self._test( self.line, 'uniform2', [[.25],[.75]], [.5,.5] )
-    self._test( self.line, 'uniform3', [[1/6.],[1/2.],[5/6.]], [1/3.,1/3.,1/3.] )
+    self._test( self.line, 'uniform3', [[1/6.],[1/2.],[5/6.]], [1/3.]*3 )
     self._test( self.triangle, 'uniform1', [[1/3.,1/3.]], [.5] )
-    self._test( self.triangle, 'uniform2', [[1/6.,1/6.],[1/6.,2/3.],[2/3.,1/6.],[1/3.,1/3.]], [1/8.,1/8.,1/8.,1/8.] )
+    self._test( self.triangle, 'uniform2', [[1/6.,1/6.],[1/6.,2/3.],[2/3.,1/6.],[1/3.,1/3.]], [1/8.]*4 )
     self._test( self.quad, 'uniform1', [[.5,.5]], [1.] )
-    self._test( self.quad, 'uniform2', [[.25,.25],[.25,.75],[.75,.25],[.75,.75]], [.25,.25,.25,.25] )
-    self._test( self.quad, 'uniform1,3', [[.5,1/6.],[.5,1/2.],[.5,5/6.]], [1/3.,1/3.,1/3.] )
+    self._test( self.quad, 'uniform2', [[.25,.25],[.25,.75],[.75,.25],[.75,.75]], [1/4.]*4 )
+    self._test( self.quad, 'uniform1,3', [[.5,1/6.],[.5,1/2.],[.5,5/6.]], [1/3.]*3 )
     self._test( self.hexagon, 'uniform1', [[.5,.5,.5]], [1.] )
+    self._test( self.prism, 'uniform1', [[.5,1/3.,1/3.]], [.5] )
+    self._test( self.prism, 'uniform2', [[.25,1/6.,1/6.],[.25,1/6.,2/3.],[.25,2/3.,1/6.],[.25,1/3.,1/3.],[.75,1/6.,1/6.],[.75,1/6.,2/3.],[.75,2/3.,1/6.],[.75,1/3.,1/3.]], [1/16.]*8 )
 
   def testMixed( self ):
     a = numpy.sqrt(1/3.)
     self._test( self.quad, 'uniform1*gauss1', [[.5,.5]], [1.] )
     self._test( self.quad, 'uniform2*gauss1', [[.25,.5],[.75,.5]], [.5,.5] )
     self._test( self.quad, 'uniform1*gauss2', [[.5,.5-.5*a],[.5,.5+.5*a]], [.5,.5] )
-    self._test( self.quad, 'uniform2*gauss2', [[.25,.5-.5*a],[.25,.5+.5*a],[.75,.5-.5*a],[.75,.5+.5*a]], [.25,.25,.25,.25] )
+    self._test( self.quad, 'uniform2*gauss2', [[.25,.5-.5*a],[.25,.5+.5*a],[.75,.5-.5*a],[.75,.5+.5*a]], [1/4.]*4 )
+    self._test( self.prism, 'uniform1*gauss2', [[.5,2/3.,1/6.],[.5,1/6.,2/3.],[.5,1/6.,1/6.]], [1/6.]*3 )
