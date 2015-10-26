e1639a2ebb37d584b967b8ef283bd695d5b3dec1
Gertjan van Zwieten <gertjanvanzwieten@gmail.com>
2015-10-26 16:05:12 +0100

    fix bug in NanVec boolean-and operation

--- nutils/util.py
+++ nutils/util.py
@@ -210,7 +210,6 @@ class NanVec( numpy.ndarray ):
       self[ where ] = other
     else:
       assert isinstance( other, numpy.ndarray ) and other.shape == self.shape
-      where &= ~numpy.isnan( other.view(numpy.ndarray) )
       self[ where ] = other[ where ]
     return self
 
