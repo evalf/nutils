72094c29782a1e794a9a420c5a19506a8c7148fb
Joost van Zwieten <jvanzwieten@hvzengineering.nl>
2016-02-29 12:31:30 +0100

    test python 3.3 (lowest supported), 3.4 and 3.5

--- nutils/__init__.py
+++ nutils/__init__.py
@@ -1,6 +1,7 @@
-import numpy
+import sys, numpy
 from distutils.version import LooseVersion
 
+assert sys.version_info >= (3, 3)
 assert LooseVersion(numpy.version.version) >= LooseVersion('1.8'), 'nutils requires numpy 1.8 or higher, got %s' % numpy.version.version
 
 version = '3beta'
