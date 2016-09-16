84d873bf4e7f63033d62d8e39a5b88d985740c13
Gertjan van Zwieten <gjvanzwieten@hvzengineering.nl>
2016-09-16 17:51:40 +0200

    add rect+iface unittest

--- tests/mesh.py
+++ tests/mesh.py
@@ -88,7 +88,7 @@ def gmsh():
   for group, exact_length in ('neumann',1), ('dirichlet',3), ((),4):
     @unittest( name=group or 'all' )
     def length():
-      length = domain.boundary[group].integrate( 1., geometry=geom, ischeme='gauss1' )
+      length = domain.boundary[group].integrate( 1, geometry=geom, ischeme='gauss1' )
       numpy.testing.assert_almost_equal( length, exact_length, decimal=10 )
 
   @unittest
@@ -99,10 +99,154 @@ def gmsh():
   @unittest
   def divergence():
     volumes = domain.boundary.integrate( geom*geom.normal(), geometry=geom, ischeme='gauss1' )
-    numpy.testing.assert_almost_equal( volumes, 1., decimal=10 )
+    numpy.testing.assert_almost_equal( volumes, 1, decimal=10 )
 
   @unittest
   def pointeval():
     xy = domain.points.elem_eval( geom, ischeme='gauss1' )
     assert xy.shape == ( 2, 2 )
     assert numpy.all( xy == [1,0] )
+
+# gmshrect geo:
+#
+# Point(1) = {0,0,0,.5};
+# Point(2) = {1,0,0,.5};
+# Point(3) = {2,0,0,.5};
+# Point(4) = {0,1,0,.5};
+# Point(5) = {1,1,0,.5};
+# Point(6) = {2,1,0,.5};
+# Line(7) = {1,2};
+# Line(8) = {2,3};
+# Line(9) = {3,6};
+# Line(10) = {6,5};
+# Line(11) = {5,4};
+# Line(12) = {4,1};
+# Line(13) = {2,5};
+# Line Loop(14) = {7,13,11,12};
+# Line Loop(15) = {8,9,10,-13};
+# Plane Surface(16) = {14};
+# Plane Surface(17) = {15};
+# Physical Line("iface") = {13};
+# Physical Surface("left") = {16};
+# Physical Surface("right") = {17};
+
+gmshrectdata = '''\
+$MeshFormat
+2.2 0 8
+$EndMeshFormat
+$PhysicalNames
+3
+1 1 "iface"
+2 2 "left"
+2 3 "right"
+$EndPhysicalNames
+$Nodes
+23
+1 0 0 0
+2 1 0 0
+3 2 0 0
+4 0 1 0
+5 1 1 0
+6 2 1 0
+7 0.499999999998694 0 0
+8 1.5 0 0
+9 2 0.499999999998694 0
+10 1.5 1 0
+11 0.5000000000020591 1 0
+12 0 0.5000000000020591 0
+13 1 0.499999999998694 0
+14 0.5 0.4999999999999999 0
+15 0.2500000000010295 0.7500000000010295 0
+16 0.7500000000005148 0.7499999999996735 0
+17 0.2499999999989704 0.2500000000010296 0
+18 0.7499999999996735 0.2499999999996735 0
+19 1.5 0.49999999999983 0
+20 1.75 0.249999999999347 0
+21 1.25 0.249999999999347 0
+22 1.25 0.749999999999631 0
+23 1.75 0.749999999999631 0
+$EndNodes
+$Elements
+34
+1 1 2 1 13 2 13
+2 1 2 1 13 13 5
+3 2 2 2 16 17 15 12
+4 2 2 2 16 14 15 17
+5 2 2 2 16 4 15 11
+6 2 2 2 16 1 17 12
+7 2 2 2 16 5 16 13
+8 2 2 2 16 2 18 7
+9 2 2 2 16 11 15 14
+10 2 2 2 16 11 14 16
+11 2 2 2 16 13 16 14
+12 2 2 2 16 13 14 18
+13 2 2 2 16 7 14 17
+14 2 2 2 16 7 18 14
+15 2 2 2 16 1 7 17
+16 2 2 2 16 2 13 18
+17 2 2 2 16 4 12 15
+18 2 2 2 16 5 11 16
+19 2 2 3 17 22 13 19
+20 2 2 3 17 9 19 20
+21 2 2 3 17 13 21 19
+22 2 2 3 17 23 19 9
+23 2 2 3 17 8 20 19
+24 2 2 3 17 21 8 19
+25 2 2 3 17 5 13 22
+26 2 2 3 17 6 23 9
+27 2 2 3 17 2 8 21
+28 2 2 3 17 3 20 8
+29 2 2 3 17 10 22 19
+30 2 2 3 17 10 19 23
+31 2 2 3 17 5 22 10
+32 2 2 3 17 6 10 23
+33 2 2 3 17 2 21 13
+34 2 2 3 17 3 9 20
+$EndElements
+'''
+
+@register
+def gmshrect():
+
+  domain, geom = mesh.gmsh( gmshrectdata.splitlines() )
+
+  @unittest
+  def volume():
+    volume = domain.integrate( 1, geometry=geom, ischeme='gauss1' )
+    numpy.testing.assert_almost_equal( volume, 2, decimal=10 )
+
+  @unittest
+  def length():
+    length = domain.boundary.integrate( 1, geometry=geom, ischeme='gauss1' )
+    numpy.testing.assert_almost_equal( length, 6, decimal=10 )
+
+  @unittest
+  def divergence():
+    volumes = domain.boundary.integrate( geom*geom.normal(), geometry=geom, ischeme='gauss1' )
+    numpy.testing.assert_almost_equal( volumes, 2, decimal=10 )
+
+  for group in 'left', 'right':
+    subdom = domain[group]
+    @unittest( name=group )
+    def subvolume():
+      volume = subdom.integrate( 1, geometry=geom, ischeme='gauss1' )
+      numpy.testing.assert_almost_equal( volume, 1, decimal=10 )
+    @unittest( name=group )
+    def sublength():
+      length = subdom.boundary.integrate( 1, geometry=geom, ischeme='gauss1' )
+      numpy.testing.assert_almost_equal( length, 4, decimal=10 )
+    @unittest( name=group )
+    def subdivergence():
+      volumes = subdom.boundary.integrate( geom*geom.normal(), geometry=geom, ischeme='gauss1' )
+      numpy.testing.assert_almost_equal( volumes, 1, decimal=10 )
+
+  @unittest
+  def iface():
+    ax, ay = domain.interfaces['iface'].elem_eval( geom, ischeme='uniform1', separate=False ).T
+    bx, by = domain['left'].boundary['~iface'].elem_eval( geom, ischeme='uniform1', separate=False ).T
+    cx, cy = domain['right'].boundary['iface'].elem_eval( geom, ischeme='uniform1', separate=False ).T
+    assert all( ax == 1 )
+    assert all( bx == 1 )
+    assert all( cx == 1 )
+    assert min(ay) == min(by) == min(cy)
+    assert max(ay) == max(by) == max(cy)
