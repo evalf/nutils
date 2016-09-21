e1100e94ee256356a300a689ae7861b659e44b3b
Gertjan van Zwieten <gjvanzwieten@hvzengineering.nl>
2016-09-21 18:45:18 +0200

    add periodicity unit test

--- tests/mesh.py
+++ tests/mesh.py
@@ -250,3 +250,140 @@ def gmshrect():
     assert all( cx == 1 )
     assert min(ay) == min(by) == min(cy)
     assert max(ay) == max(by) == max(cy)
+
+# gmshperiodic geo:
+#
+# Point(1) = {0,0,0,.4};
+# Point(2) = {1,0,0,.4};
+# Point(3) = {1,1,0,.4};
+# Point(4) = {0,1,0,.4};
+# Line(5) = {1,2};
+# Line(6) = {2,3};
+# Line(7) = {3,4};
+# Line(8) = {4,1};
+# Line Loop(9) = {5,6,7,8};
+# Plane Surface(10) = {9};
+# Physical Surface("interior") = {10};
+# Physical Line("right") = {6};
+# Physical Line("left") = {8};
+# Physical Line("periodic") = {5};
+# Periodic Line { 5 } = { -7 };
+
+gmshperiodicdata = '''\
+$MeshFormat
+2.2 0 8
+$EndMeshFormat
+$PhysicalNames
+4
+1 2 "right"
+1 3 "left"
+1 4 "periodic"
+2 1 "interior"
+$EndPhysicalNames
+$Nodes
+21
+1 0 0 0
+2 1 0 0
+3 1 1 0
+4 0 1 0
+5 0.3333333333347203 0 0
+6 0.6666666666675911 0 0
+7 1 0.3333333333325025 0
+8 1 0.6666666666657889 0
+9 0.6666666666675911 1 0
+10 0.3333333333347203 1 0
+11 0 0.6666666666675911 0
+12 0 0.3333333333347203 0
+13 0.5000000000000648 0.5000000000000175 0
+14 0.7083333333332844 0.2916666666667275 0
+15 0.2916666666667967 0.7083333333334922 0
+16 0.25099206349244 0.2509920634924497 0
+17 0.7083333333334444 0.7083333333332192 0
+18 0.1726190476198807 0.5000000000000979 0
+19 0.5000000000001986 0.8273809523802892 0
+20 0.8273809523805672 0.4999999999999279 0
+21 0.4999999999999743 0.172619047619863 0
+$EndNodes
+$Elements
+37
+1 1 2 4 5 1 5
+2 1 2 4 5 5 6
+3 1 2 4 5 6 2
+4 1 2 2 6 2 7
+5 1 2 2 6 7 8
+6 1 2 2 6 8 3
+7 1 2 3 8 4 11
+8 1 2 3 8 11 12
+9 1 2 3 8 12 1
+10 2 2 1 10 4 15 10
+11 2 2 1 10 2 14 6
+12 2 2 1 10 2 7 14
+13 2 2 1 10 4 11 15
+14 2 2 1 10 3 9 17
+15 2 2 1 10 3 17 8
+16 2 2 1 10 1 16 12
+17 2 2 1 10 1 5 16
+18 2 2 1 10 13 14 20
+19 2 2 1 10 13 20 17
+20 2 2 1 10 13 19 15
+21 2 2 1 10 13 17 19
+22 2 2 1 10 13 21 14
+23 2 2 1 10 13 15 18
+24 2 2 1 10 13 16 21
+25 2 2 1 10 13 18 16
+26 2 2 1 10 7 8 20
+27 2 2 1 10 5 6 21
+28 2 2 1 10 11 12 18
+29 2 2 1 10 9 10 19
+30 2 2 1 10 11 18 15
+31 2 2 1 10 6 14 21
+32 2 2 1 10 9 19 17
+33 2 2 1 10 7 20 14
+34 2 2 1 10 12 16 18
+35 2 2 1 10 5 21 16
+36 2 2 1 10 10 15 19
+37 2 2 1 10 8 17 20
+$EndElements
+$Periodic
+3
+0 1 4
+1
+1 4
+0 2 3
+1
+2 3
+1 5 7
+4
+2 3
+1 4
+5 10
+6 9
+$EndPeriodic
+'''
+
+@register
+def gmshperiodic():
+
+  domain, geom = mesh.gmsh( gmshperiodicdata.splitlines() )
+
+  @unittest
+  def volume():
+    volume = domain.integrate( 1, geometry=geom, ischeme='gauss1' )
+    numpy.testing.assert_almost_equal( volume, 1, decimal=10 )
+
+  for group, exact_length in ('right',1), ('left',1), ((),2):
+    @unittest( name=group or 'all' )
+    def length():
+      length = domain.boundary[group].integrate( 1, geometry=geom, ischeme='gauss1' )
+      numpy.testing.assert_almost_equal( length, exact_length, decimal=10 )
+
+  @unittest
+  def interface():
+    err = domain.interfaces['periodic'].elem_eval( geom - function.opposite(geom), ischeme='uniform2', separate=False )
+    numpy.testing.assert_almost_equal( err-[0,1], 0, decimal=15 )
+
+  @unittest
+  def basis():
+    basis = domain.basis( 'std', degree=1 )
+    err = domain.interfaces.integrate( basis - function.opposite(basis), geometry=geom, ischeme='gauss1' )
+    numpy.testing.assert_almost_equal( err, 0, decimal=15 )
