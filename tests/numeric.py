428c16a3c545e24fc09e58a7a5ed3406dfb25e7a
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2017-02-01 22:35:47 +0100

    add numeric.searchsorted

--- tests/numeric.py
+++ tests/numeric.py
@@ -15,3 +15,22 @@ def ortho_complement( m, n ):
   @unittest
   def orthogonal():
     numpy.testing.assert_almost_equal( numpy.dot( A.T, B ), 0, decimal=15 )
+
+
+@register
+def searchsorted():
+  for i in range(4):
+    @unittest( name=i )
+    def insertion():
+      assert numeric.searchsorted( [.5,1.5,2.5], i ) == i
+  for i in range(3):
+    @unittest( name=i )
+    def lookup():
+      assert numeric.searchsorted( [0,1,2], i ) == i
+  @unittest
+  def repeats():
+    assert numeric.searchsorted( [.5,.5,.5], 0 ) == 0
+    assert numeric.searchsorted( [.5,.5,.5], 1 ) == 3
+  @unittest
+  def strings():
+    assert numeric.searchsorted( ['bar','foo','fool'], 'food' ) == 2
