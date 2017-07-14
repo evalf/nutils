5ec6bf78bd5b175fe89d6d9a651da3949db2d443
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2017-07-14 13:56:38 +0200

    update cahnhilliard example

--- examples/cahnhilliard.py
+++ examples/cahnhilliard.py
@@ -1,37 +1,37 @@
 #! /usr/bin/env python3
 
-from nutils import mesh, plot, cli, log, library, function, numeric, debug
+from nutils import mesh, plot, cli, log, function, numeric, debug, model
 import numpy
 
 
 class MakePlots( object ):
 
-  def __init__( self, domain, geom, nsteps, video ):
+  def __init__(self, domain, nsteps, namespace):
     self.domain = domain
-    self.geom = geom
-    self.energies = numpy.empty( (nsteps,3) )
-    self.plt = video and plot.PyPlotVideo( 'concentration' )
+    self.namespace = namespace
+    self.energies = numpy.empty((nsteps, 4))
     self.index = 0
 
-  def __call__( self, c, mu, *energies ):
-    self.energies[self.index] = energies
-    self.index += 1
-    xpnt, cpnt = self.domain.elem_eval( [ self.geom, c ], ischeme='bezier4', separate=True )
-    I = numpy.arange(self.index-1,-1,-1)
-    E = self.energies[self.index-1::-1].T
-    with self.plt if self.plt else plot.PyPlot( 'flow', index=self.index ) as plt:
-      plt.axes( yticks=[], xticks=[] )
-      plt.mesh( xpnt, cpnt )
+  def __call__(self, lhs):
+    ns = self.namespace | dict(lhs=lhs)
+    self.energies[self.index,:2] = self.domain.integrate(['(c^2 - 1)^2 / 2 epsilon^2' @ ns, '.5 c_,k c_,k' @ ns], geometry=ns.x, degree=4)
+    self.energies[self.index,2] = self.domain.boundary.integrate('abs(ewall) + ewall c' @ ns, geometry=ns.x, degree=4)
+    self.energies[self.index,3] = self.energies[self.index,:3].sum()
+    x, c = self.domain.elem_eval([ns.x, ns.c], ischeme='bezier4', separate=True)
+    with plot.PyPlot('flow', index=self.index) as plt:
+      plt.axes(yticks=[], xticks=[])
+      plt.mesh(x, c)
       plt.colorbar()
-      plt.clim( -1, 1 )
-      plt.axes( [.07,.05,.35,.25], yticks=[], xticks=[], axisbg='w' ).patch.set_alpha( .8 )
-      for i, name in enumerate(['mixture','interface','wall','total']):
-        plt.plot( I, E[i] if i < len(E) else E.sum(0), '-o', markevery=self.index, label=name )
+      plt.clim(-1, 1)
+      plt.axes([.07,.05,.35,.25], yticks=[], xticks=[], axisbg='w').patch.set_alpha(.8)
+      for energy, name in zip(self.energies[:self.index+1].T, ['mixture','interface','wall','total']):
+        plt.plot(numpy.arange(len(energy))[::-1], energy[::-1], '-o', markevery=self.index+1, label=name)
       plt.legend( numpoints=1, frameon=False, fontsize=8 )
-      plt.xlim( 0, len(self.energies) )
-      plt.ylim( 0, self.energies[0].sum() )
-      plt.xlabel( 'time' )
-      plt.ylabel( 'energy' )
+      plt.xlim(0, len(self.energies))
+      plt.ylim(0, self.energies[0,3])
+      plt.xlabel('time')
+      plt.ylabel('energy')
+    self.index += 1
 
 
 def main(
@@ -46,85 +46,79 @@ def main(
 
   mineps = 1./nelems
   if not epsilon:
-    log.info( 'setting epsilon=%f' % mineps )
+    log.info('setting epsilon={}'.format(mineps))
     epsilon = mineps
   elif epsilon < mineps:
-    log.warning( 'epsilon under crititical threshold: %f < %f' % ( epsilon, mineps ) )
+    log.warning('epsilon under crititical threshold: {} < {}'.format(epsilon, mineps))
 
-  ewall = .5 * numpy.cos( theta * numpy.pi / 180 )
+  # create namespace
+  ns = function.Namespace()
+  ns.epsilon = epsilon
+  ns.ewall = .5 * numpy.cos( theta * numpy.pi / 180 )
 
   # construct mesh
   xnodes = ynodes = numpy.linspace(0,1,nelems+1)
-  domain, geom = mesh.rectilinear( [ xnodes, ynodes ] )
+  domain, ns.x = mesh.rectilinear( [ xnodes, ynodes ] )
 
   # prepare bases
-  cbasis, mubasis = function.chain([ domain.basis( 'spline', degree=2 ), domain.basis( 'spline', degree=2 ) ])
+  ns.cbasis, ns.mubasis = function.chain([
+    domain.basis('spline', degree=2),
+    domain.basis('spline', degree=2)
+  ])
 
-  # define mixing energy and splitting: F' = f_p - f_n
-  F = lambda c_: (.5/epsilon**2) * (c_**2-1)**2
-  f_p = lambda c_: (1./epsilon**2) * 4*c_
-  f_n = lambda c_: (1./epsilon**2) * ( 6*c_ - 2*c_**3 )
-
-  # prepare matrix
-  A = function.outer( cbasis ) \
-    + (timestep*epsilon**2) * function.outer( cbasis.grad(geom), mubasis.grad(geom) ).sum(-1) \
-    + function.outer( mubasis, mubasis - f_p(cbasis) ) \
-    - function.outer( mubasis.grad(geom), cbasis.grad(geom) ).sum(-1)
-  matrix = domain.integrate( A, geometry=geom, ischeme='gauss4' )
-
-  # prepare wall energy right hand side
-  rhs0 = domain.boundary.integrate( mubasis * ewall, geometry=geom, ischeme='gauss4' )
+  # polulate namespace
+  ns.c = 'cbasis_n ?lhs_n'
+  ns.c0 = 'cbasis_n ?lhs0_n'
+  ns.mu = 'mubasis_n ?lhs_n'
+  ns.f = '(6 c0 - 2 c0^3 - 4 c) / epsilon^2'
 
   # construct initial condition
   if init == 'random':
-    numpy.random.seed( 0 )
-    c = cbasis.dot( numpy.random.normal(0,.5,cbasis.shape) )
+    numpy.random.seed(0)
+    lhs0 = numpy.random.normal(0, .5, ns.cbasis.shape)
   elif init == 'bubbles':
     R1 = .25
     R2 = numpy.sqrt(.5) * R1 # area2 = .5 * area1
-    c = 1 + function.tanh( (R1-function.norm2(geom-(.5+R2/numpy.sqrt(2)+.8*epsilon)))/epsilon ) \
-          + function.tanh( (R2-function.norm2(geom-(.5-R1/numpy.sqrt(2)-.8*epsilon)))/epsilon )
+    ns.cbubble1 = function.tanh((R1-function.norm2(ns.x-(.5+R2/numpy.sqrt(2)+.8*ns.epsilon)))/ns.epsilon)
+    ns.cbubble2 = function.tanh((R2-function.norm2(ns.x-(.5-R1/numpy.sqrt(2)-.8*ns.epsilon)))/ns.epsilon)
+    sqr = domain.integral('(c - cbubble1 - cbubble2 - 1)^2 + mu^2' @ ns, geometry=ns.x, degree=4)
+    lhs0 = model.optimize('lhs', sqr)
   else:
     raise Exception( 'unknown init %r' % init )
 
-  # prepare plotting
+  # construct residual
+  res = domain.integral('epsilon^2 cbasis_n,k mu_,k + mubasis_n (mu + f) - mubasis_n,k c_,k' @ ns, geometry=ns.x, degree=4)
+  res -= domain.boundary.integral('mubasis_n ewall' @ ns, geometry=ns.x, degree=4)
+  inertia = domain.integral('cbasis_n c' @ ns, geometry=ns.x, degree=4)
+
+  # solve time dependent problem
   nsteps = numeric.round(maxtime/timestep)
-  makeplots = MakePlots( domain, geom, nsteps, video=withplots=='video' ) if withplots else lambda *args: None
+  makeplots = MakePlots(domain, nsteps, ns) if withplots else lambda *args: None
+  for istep, lhs in log.enumerate('timestep', model.impliciteuler('lhs', target0='lhs0', residual=res, inertia=inertia, timestep=timestep, lhs0=lhs0)):
+    makeplots(lhs)
+    if istep == nsteps:
+      break
 
-  # start time stepping
-  for istep in log.range( 'timestep', nsteps ):
-
-    Emix = F(c)
-    Eiface = .5 * (c.grad(geom)**2).sum(-1)
-    Ewall = ( abs(ewall) + ewall * c )
-
-    b = cbasis * c - mubasis * f_n(c)
-    rhs, total, energy_mix, energy_iface = domain.integrate( [ b, c, Emix, Eiface ], geometry=geom, ischeme='gauss4' )
-    energy_wall = domain.boundary.integrate( Ewall, geometry=geom, ischeme='gauss4' )
-    log.user( 'concentration {}, energy {}'.format( total, energy_mix + energy_iface + energy_wall ) )
-
-    lhs = matrix.solve( rhs0 + rhs, tol=1e-12, restart=999 )
-    c = cbasis.dot( lhs )
-    mu = mubasis.dot( lhs )
-
-    makeplots( c, mu, energy_mix, energy_iface, energy_wall )
-
-  return lhs, energy_mix, energy_iface, energy_wall
+  return lhs0, lhs
 
 
 def unittest():
 
-  retvals = main( nelems=8, init='bubbles', maxtime=.1, withplots=False )
+  retvals = main(nelems=8, init='bubbles', timestep=.01, maxtime=.05, withplots=False)
   assert debug.checkdata( retvals, '''
-    eNptk9lxwzAMRNuxZ6QMARJXQem/hVC4bDn+AihRj7sLCo7HOoCex+OhJPJ7rkNJ6aqCYFelBfp70nHK
-    WPN6cKpN33HahHhiC0c2jLemaEoGQV1+CsOCpE4o6ioqF5W+U0tb0UhQfQPr4MCiYWIh+IYkyWdL2pRq
-    +N1qi2vcEgynohritI7cutP74GxgYTVhoKNrt63vBayvhCyCYBbLZsZZAgrV6H0WHV8bboUNnBBAsIgP
-    JSqQaNSJH4PtWVR6bbfUFUxk+Sm6yNeKEYcirs9LUnPtSVR05bSUFcwGuSm1hFvm+O9qvE21A83UymYr
-    K5iESeW+GF+g/DHSnEBlVjZLWcGU0d8TgL8nJv+OyHadB4Dufbg5QFEvzq4Lwesc4hUvBV59X3EYbCZX
-    gmecvOQiJw+Tpx88r6WnOYPjfEp9AiPW13nOSw5HBbZ7ze9aT67VslL4Yg5dK/Xug7yOYVn1ng/c9RRH
-    Z+xjTZ/i+01oXn7YBnpV9XXnXPmUr9JTHMq1D37nJBL5SPD2fvJa86qcK5/ylXqa47/N3D/lWHGVF8nw
-    J4shJNboe2QVdURU1krSC7W1u7gTgNzV1di3sfe4Mu6Kqey9VL1QEk53M+H9Sn5hut6OPKNqg62qUbtZ
-    3jz318sHTDOUjJ/j+QdkFXSp''' )
+    eNrtVkGSGzEI/M5ulSclhBDwoP3/FyIJmrEnOeQB2XIV7FjT6m5AMr2+xovk+/X1dVmT9nON1zWtRcKs
+    epIhPk5iZHQSn2yRmHEkLj0TdyTjAejtfNVnxMEyfy5ZK9wTbnC9HE+otYXC+4nWBvzBD3BrKe+lRj33
+    az3RemrzARBjoBGSaR9qwQ5w67HEgp4umGo8cbLEZ8sn0kK8K89P70ps0StAZY/XJ42ea2zkmgRUm7m7
+    THkUo9yD3mIIwK5+omo/cUpu5LQNWbWQbn+UtqoD96AX9ADn088XqxlOHCM9o9vv7JO7sPUVzINWkAOa
+    t9h/GRfbd3N6Np0+6ypP4yAV5AqOV7GyY4U+e7iartrkLizqUM6V2OJXgNbGeav9+v/51886npSFd8m8
+    s+7Iw8/o03SOE0QsLDdDJ7A4GiBLJziiMgEarb/TCBqtwsx5LskwoCYGY+SFCrUhOU/ADWhieUxpzMYl
+    aBjTaIY1rw0T1kGR5COBUnADWlceMbfesx/71IS3dKDlFK59kmvP6SjfSmqxK8CpCTgpLwTBySWeNHVk
+    y+vIYapClHcltxgCkGTPyZrIGWx4tlMQmXsgdU2U9KjzXdUqBLwruWAHNBvxvxEdarPHUE9KgW8dgqJW
+    HWAdlIIawDQvH6Hc1Hv7o9vao6YoA1yDTjADGI9dC9k3G4X8N1B+gtKjAjANOkGt0KhpJrbNXGcx6b7z
+    +CUtIpGt2F809nY7Njmxi55IFLGxZBw7FtBFkw+0dErE3RYbabZE1ES0RLQH4olgdOPk+5qMlANHWrzf
+    LZ5T6zv6qR/vSB/KwCdx1tVzFJDHe6s1Qznv9zafcfBt+uFh0wKvHAKv5AMcS57iETl0e9tX6dKl/fx4
+    kcLL9fAHusAHOENCR9P27tNFnEPWh8WQoWZwGg5BWTIC0jrGDrOrNzrU9uWVCW1IvsuPoqXZMKnEgVRB
+    USOJRpmOjml/LTwKB8Nh1C2waBVW55C2knQr+/INlN6rB9fLrJJYvAprrWnHoO/X9298ambu''')
 
 
 if __name__ == '__main__':
