be61debedee39dcc469d07068a95f726e2ddf21a
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2017-12-05 18:24:45 +0100

    embed MIT license in all source files

--- nutils/core.py
+++ nutils/core.py
@@ -1,10 +1,22 @@
-# -*- coding: utf8 -*-
+# Copyright (c) 2014 Evalf
 #
-# Module CORE
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# Part of Nutils: open source numerical utilities for Python. Jointly developed
-# by HvZ Computational Engineering, TU/e Multiscale Engineering Fluid Dynamics,
-# and others. More info at http://nutils.org <info@nutils.org>. (c) 2014
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 """
 The core module provides a collection of low level constructs that have no
