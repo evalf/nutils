dff8bc84aae87e104cbdae240a2272e3d3c41b6c
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2017-12-08 15:07:21 +0100

    reorganise cli unit tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -20,17 +20,16 @@ class run(ContextTestCase):
     super().setUpContext(stack)
     self.outrootdir = stack.enter_context(tempfile.TemporaryDirectory())
 
-  def test_good(self):
+  def _cli(self, *args, funcname='main'):
     _savestreams = sys.stdout, sys.stderr
     _saveargs = tuple(sys.argv)
     try:
       sys.stdout = sys.stderr = stringio = io.StringIO()
-      sys.argv[:] = self.scriptname, '--outrootdir='+self.outrootdir, '--nopdb', '--symlink=xyz', '--iarg=1', '--farg=1', '--sarg=1'
-      if self.method == 'run':
-        nutils.cli.run(main)
-      else:
-        sys.argv.insert(1, 'main')
-        nutils.cli.choose(main)
+      sys.argv[:] = self.scriptname,
+      if self.method == 'choose' and funcname:
+        sys.argv.append(funcname)
+      sys.argv.extend(args)
+      getattr(nutils.cli, self.method)(main)
     except SystemExit as e:
       status = e
     else:
@@ -38,83 +37,43 @@ class run(ContextTestCase):
     finally:
       sys.stdout, sys.stderr = _savestreams
       sys.argv[:] = _saveargs
+    return status, stringio.getvalue()
 
+  def test_good(self):
+    status, output = self._cli('--outrootdir='+self.outrootdir, '--nopdb', '--symlink=xyz', '--iarg=1', '--farg=1', '--sarg=1')
     with self.subTest('outdir'):
-      print(os.listdir(self.outrootdir))
       self.assertTrue(os.path.isdir(os.path.join(self.outrootdir,self.scriptname)), 'output directory not found')
-
     with self.subTest('first-symlink'):
       self.assertTrue(os.path.islink(os.path.join(self.outrootdir,'xyz')), 'first symlink not found')
     with self.subTest('second-symlink'):
       self.assertTrue(os.path.islink(os.path.join(self.outrootdir,self.scriptname,'xyz')), 'second symlink not found')
-
     with self.subTest('argparse'):
-      output = stringio.getvalue()
       nutils.log.info( output )
       self.assertIn('all OK', output)
-
     with self.subTest('exitstatus'):
       self.assertIsNotNone(status)
       self.assertEqual(status.code, 0)
 
   def test_bad(self):
-    _savestreams = sys.stdout, sys.stderr
-    _saveargs = tuple(sys.argv)
-    try:
-      sys.stdout = sys.stderr = stringio = io.StringIO()
-      sys.argv[:] = self.scriptname, '--outrootdir='+self.outrootdir, '--nopdb', '--symlink=xyz', '--iarg=1', '--farg=x', '--sarg=1'
-      if self.method == 'run':
-        nutils.cli.run(main)
-      else:
-        sys.argv.insert(1, 'main')
-        nutils.cli.choose(main)
-    except SystemExit as e:
-      status = e
-    else:
-      status = None
-    finally:
-      sys.stdout, sys.stderr = _savestreams
-      sys.argv[:] = _saveargs
-
+    status, output = self._cli('--outrootdir='+self.outrootdir, '--nopdb', '--symlink=xyz', '--iarg=1', '--farg=x', '--sarg=1')
     with self.subTest('outdir'):
       self.assertFalse(os.path.isdir(os.path.join(self.outrootdir,self.scriptname)), 'outdir directory found')
-
     with self.subTest('argparse'):
-      output = stringio.getvalue()
       nutils.log.info( output )
       self.assertNotIn('all OK', output)
-
     with self.subTest('exitstatus'):
       self.assertIsNotNone(status)
       self.assertEqual(status.code, 2)
 
   def test_help(self):
-    args = '-h', '--help'
-    if self.method == 'run':
-      method = nutils.cli.run
-    else:
-      method = nutils.cli.choose
-      args += tuple('main ' + arg for arg in args)
-    for arg in args:
-      _savestreams = sys.stdout, sys.stderr
-      _saveargs = tuple(sys.argv)
-      try:
-        sys.argv[:] = [self.scriptname] + arg.split()
-        sys.stdout = sys.stderr = stringio = io.StringIO()
-        method(main)
-      except SystemExit as e:
-        status = e
-      else:
-        status = None
-      finally:
-        sys.stdout, sys.stderr = _savestreams
-        sys.argv[:] = _saveargs
-      with self.subTest(arg=arg, test='argparse'):
-        output = stringio.getvalue()
-        self.assertEqual(output[:6], 'usage:')
-      with self.subTest(arg=arg, test='exitstatus'):
-        self.assertIsNotNone(status)
-        self.assertEqual(status.code, 1)
+    for arg in '-h', '--help':
+      for funcname in ('main', False):
+        status, output = self._cli(arg, funcname=funcname)
+        with self.subTest(arg=arg, funcname=funcname, test='argparse'):
+          self.assertEqual(output[:6], 'usage:')
+        with self.subTest(arg=arg, funcname=funcname, test='exitstatus'):
+          self.assertIsNotNone(status)
+          self.assertEqual(status.code, 1)
 
 run(method='run')
 run(method='choose')
