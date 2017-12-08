8be9138822f4f611c3a3032c8aae8eee0cce9fed
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2017-12-08 15:36:47 +0100

    add badarg unittest

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -54,7 +54,16 @@ class run(ContextTestCase):
       self.assertIsNotNone(status)
       self.assertEqual(status.code, 0)
 
-  def test_bad(self):
+  def test_badarg(self):
+    status, output = self._cli('--bla') if self.method == 'run' else self._cli(funcname='bla')
+    with self.subTest('argparse'):
+      nutils.log.info( output )
+      self.assertNotIn('all OK', output)
+    with self.subTest('exitstatus'):
+      self.assertIsNotNone(status)
+      self.assertEqual(status.code, 2)
+
+  def test_badvalue(self):
     status, output = self._cli('--outrootdir='+self.outrootdir, '--nopdb', '--symlink=xyz', '--iarg=1', '--farg=x', '--sarg=1')
     with self.subTest('outdir'):
       self.assertFalse(os.path.isdir(os.path.join(self.outrootdir,self.scriptname)), 'outdir directory found')
