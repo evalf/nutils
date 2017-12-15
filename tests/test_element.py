c70a94ba9ea665cf6eedea71b37e1fc9209376e6
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2017-12-15 21:50:04 +0100

    change element unittest parameterization

--- tests/test_element.py
+++ tests/test_element.py
@@ -5,24 +5,18 @@ import itertools
 @parametrize
 class elem(TestCase):
 
-  def setUp(self):
-    super().setUp()
-    self.ref = element.getsimplex(self.ndims[0])
-    for ndim in self.ndims[1:]:
-      self.ref *= element.getsimplex(ndim)
-
   def test_ndims(self):
-    self.assertEqual(self.ref.ndims, sum(self.ndims))
+    self.assertEqual(self.ref.ndims, len(self.exactcentroid))
 
   def test_centroid(self):
     numpy.testing.assert_almost_equal(self.ref.centroid, self.exactcentroid, decimal=15)
 
-  @parametrize.enable_if(lambda ndims, **kwargs: sum(ndims) >= 1)
+  @parametrize.enable_if(lambda ref, **kwargs: ref.ndims >= 1)
   def test_children(self):
     childvol = sum(abs(trans.det) * child.volume for trans, child in self.ref.children)
     numpy.testing.assert_almost_equal(childvol, self.ref.volume)
 
-  @parametrize.enable_if(lambda ndims, **kwargs: sum(ndims) >= 1)
+  @parametrize.enable_if(lambda ref, **kwargs: ref.ndims >= 1)
   def test_childdivide(self):
     for n in 1, 2, 3:
       with self.subTest(n=n):
@@ -31,7 +25,7 @@ class elem(TestCase):
           cpoints, cweights = cref.getischeme('vertex{}'.format(n-1))
           numpy.testing.assert_equal(ctrans.apply(cpoints), vals)
 
-  @parametrize.enable_if(lambda ndims, **kwargs: sum(ndims) >= 1)
+  @parametrize.enable_if(lambda ref, **kwargs: ref.ndims >= 1)
   def test_swap(self):
     for iedge, (etrans, eref) in enumerate(self.ref.edges):
       for ichild, (ctrans, cref) in enumerate(eref.children):
@@ -42,11 +36,11 @@ class elem(TestCase):
         swapped_down = etrans_.swapdown(ctrans_)
         self.assertEqual(swapped_down, (etrans, ctrans))
 
-  @parametrize.enable_if(lambda ndims, **kwargs: sum(ndims) >= 2)
+  @parametrize.enable_if(lambda ref, **kwargs: ref.ndims >= 2)
   def test_ribbons(self):
     self.ref.ribbons
 
-  @parametrize.enable_if(lambda ndims, **kwargs: sum(ndims) >= 1)
+  @parametrize.enable_if(lambda ref, **kwargs: ref.ndims >= 1)
   def test_connectivity(self):
     childmap, edgemap = self.ref.connectivity
     for ichild, edges in enumerate(childmap):
@@ -63,22 +57,26 @@ class elem(TestCase):
           self.ref.child_transforms[jchild] * self.ref.child_refs[jchild].edge_transforms[jedge])
 
   def test_dof_transpose_map(self):
-    nverts = tuple(element.getsimplex(ndim).nverts for ndim in self.ndims)
-    i = numpy.arange(util.product(nverts)).reshape(nverts)
-    for perm_ref_order, *perms_refs in itertools.product(*(itertools.permutations(range(n)) for n in (len(self.ndims),)+nverts)):
-        j = i
-        for k, perm in enumerate(perms_refs):
-          j = numpy.take(j, perm, axis=k)
-        j = tuple(numpy.transpose(j, perm_ref_order).ravel())
-        with self.subTest(perm_ref_order=perm_ref_order, perms_refs=perms_refs):
-          self.assertEqual(j, tuple(self.ref.get_dof_transpose_map(1, j)))
+    nverts = []
+    ref = self.ref
+    while isinstance(ref, element.TensorReference):
+      nverts.append(ref.ref1.nverts)
+      ref = ref.ref2
+    nverts.append(ref.nverts)
+    for perm_ref_order, *perms_refs in itertools.product(*(itertools.permutations(range(n)) for n in [len(nverts)]+nverts)):
+      j = numpy.arange(self.ref.nverts).reshape(nverts)
+      for k, perm in enumerate(perms_refs):
+        j = numpy.take(j, perm, axis=k)
+      j = tuple(numpy.transpose(j, perm_ref_order).ravel())
+      with self.subTest(perm_ref_order=perm_ref_order, perms_refs=perms_refs):
+        self.assertEqual(j, tuple(self.ref.get_dof_transpose_map(1, j)))
 
-elem('point', ndims=[0], exactcentroid=numpy.zeros((0,)))
-elem('point2', ndims=[0,0], exactcentroid=numpy.zeros((0,)))
-elem('line', ndims=[1], exactcentroid=[.5])
-elem('triangle', ndims=[2], exactcentroid=[1/3]*2)
-elem('tetrahedron', ndims=[3], exactcentroid=[1/4]*3)
-elem('square', ndims=[1,1], exactcentroid=[.5]*2)
-elem('hexagon', ndims=[1,1,1], exactcentroid=[.5]*3)
-elem('prism1', ndims=[2,1], exactcentroid=[1/3,1/3,1/2])
-elem('prism2', ndims=[1,2], exactcentroid=[1/2,1/3,1/3])
+elem('point', ref=element.PointReference(), exactcentroid=numpy.zeros((0,)))
+elem('point2', ref=element.PointReference()**2, exactcentroid=numpy.zeros((0,)))
+elem('line', ref=element.LineReference(), exactcentroid=[.5])
+elem('triangle', ref=element.TriangleReference(), exactcentroid=[1/3]*2)
+elem('tetrahedron', ref=element.TetrahedronReference(), exactcentroid=[1/4]*3)
+elem('square', ref=element.LineReference()**2, exactcentroid=[.5]*2)
+elem('hexagon', ref=element.LineReference()**3, exactcentroid=[.5]*3)
+elem('prism1', ref=element.TriangleReference()*element.LineReference(), exactcentroid=[1/3,1/3,1/2])
+elem('prism2', ref=element.LineReference()*element.TriangleReference(), exactcentroid=[1/2,1/3,1/3])
