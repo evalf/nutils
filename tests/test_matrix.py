1497c203374e30951c155973b99429307f312186
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2018-02-15 19:26:32 +0100

    run matrix tests for all available backends

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -1,22 +1,34 @@
-import numpy, scipy.sparse
+import numpy
 from nutils import matrix
 from . import *
 
 @parametrize
 class solver(TestCase):
 
+  ifsupported = parametrize.skip_if(lambda backend, args: not hasattr(matrix, backend), reason='not supported')
+
   def setUp(self):
     super().setUp()
+    self._backend = matrix.backend(self.backend)
+    self._backend.__enter__()
     n = 100
-    self.matrix = matrix.ScipyMatrix(scipy.sparse.spdiags(numpy.repeat([[-1],[2],[-1]], n, axis=1), [-1,0,1], n, n).tocsr())
+    r = numpy.arange(n)
+    index = numpy.concatenate([[r, r], [r[:-1], r[1:]], [r[1:], r[:-1]]], axis=1)
+    data = numpy.hstack([2.] * n + [-1.] * (2*n-2))
+    self.matrix = matrix.assemble(data, index, shape=(n,n))
     self.tol = self.args.get('atol', 1e-10)
 
+  def tearDown(self):
+    self._backend.__exit__(None, None, None)
+
+  @ifsupported
   def test_solve(self):
     rhs = numpy.arange(self.matrix.shape[0])
     lhs = self.matrix.solve(rhs, **self.args)
     res = numpy.linalg.norm(self.matrix.matvec(lhs) - rhs)
     self.assertLess(res, self.tol)
 
+  @ifsupported
   def test_constraints(self):
     cons = numpy.empty(self.matrix.shape[0])
     cons[:] = numpy.nan
@@ -29,7 +41,9 @@ class solver(TestCase):
     res = numpy.linalg.norm(self.matrix.matvec(lhs)[1:-1])
     self.assertLess(res, self.tol)
 
-solver(args=dict())
-solver(args=dict(atol=1e-5, restart=100))
-solver(args=dict(atol=1e-5, symmetric=True))
-solver(args=dict(atol=1e-5, solver='lgmres'))
+solver(backend='Numpy', args=dict())
+solver(backend='Scipy', args=dict())
+solver(backend='Scipy', args=dict(atol=1e-5, solver='gmres', restart=100))
+solver(backend='Scipy', args=dict(atol=1e-5, solver='cg'))
+solver(backend='Scipy', args=dict(atol=1e-5, solver='lgmres'))
+solver(backend='MKL', args=dict())
