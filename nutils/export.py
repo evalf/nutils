d482c0e691204213f3170b05ce6339cef34ad32a
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2018-05-09 12:35:03 +0200

    add export.vtk, test_export

--- nutils/export.py
+++ nutils/export.py
@@ -19,7 +19,7 @@
 # THE SOFTWARE.
 
 from . import config, log, core, util
-import contextlib
+import contextlib, numpy
 
 @contextlib.contextmanager
 @util.positional_only('name')
@@ -54,5 +54,100 @@ def mplfigure(*args, **kwargs):
         fig.savefig(f, format=fmt)
     fig.set_canvas(None) # break circular reference
   log.user(', '.join(name+'.'+fmt for fmt in formats))
+
+@util.positional_only('name', 'tri', 'x')
+def vtk(*args, **kwargs):
+  '''Export data to a VTK file.
+
+  This method provides a simple interface to the `VTK file format`_ with a
+  number of important restrictions:
+
+  *   Simplex-only. This makes it possible to define the mesh by a combination
+      of vertex coordinates and a connectivity table.
+  *   Legacy mode. The newer XML based format is more complex and does not
+      provide benefits within the constraints set by this method.
+  *   Binary mode. This allows for direct output of binary data, which aids
+      speed, accuracy and file size.
+
+  Beyond the mandatory file name, connectivity table, and vertex coordinates,
+  any additional data sets can be provided as keyword arguments, where the keys
+  are the names by which the arrays are stored. The data can be either vertex
+  or point data, with the distinction made based on the length of the array.
+
+  .. _`VTK file format`: https://www.vtk.org/VTK/img/file-formats.pdf
+
+  Args
+  ----
+  name : :class:`str`
+    Destination file name (without vtk extension).
+  tri : :class:`int` array
+    Triangulation.
+  x : :class:`float` array
+    Vertex coordinates.
+  **kwargs :
+    Cell and/or point data
+  '''
+
+  name, cells, points = args
+  assert cells.ndim == points.ndim == 2
+  ndims = points.shape[1]
+  assert cells.shape[1] == ndims + 1
+
+  if ndims == 2:
+    points = numpy.concatenate([points, numpy.zeros_like(points[:,:1])], axis=1)
+    celltype = 5 # VTK_TRIANGLE
+  elif ndims == 3:
+    celltype = 10 # VTK_TETRA
+  else:
+    raise Exception('invalid point dimension: {}'.format(ndims))
+
+  vtkdtype = {
+    numpy.dtype('>i1'): 'char',  numpy.dtype('>u1'): 'unsigned_char',
+    numpy.dtype('>i2'): 'short', numpy.dtype('>u2'): 'unsigned_short',
+    numpy.dtype('>i4'): 'int',   numpy.dtype('>u4'): 'unsigned_int',
+    numpy.dtype('>f4'): 'float', numpy.dtype('>f8'): 'double'}
+
+  vtkndim = {1: 'SCALARS {} {} 1\nLOOKUP_TABLE default\n', 2: 'VECTORS {} {}\n', 3: 'TENSORS {} {}\n'}
+
+  bigendian = lambda a: a.astype('>{0.kind}{0.itemsize}'.format(a.dtype))
+
+  points = bigendian(points)
+  if points.dtype not in vtkdtype:
+    raise Exception('invalid data type for points: {}'.format(points.dtype))
+
+  t_cells = numpy.empty((len(cells), ndims+2), dtype='>u4')
+  t_cells[:,0] = ndims + 1
+  t_cells[:,1:] = cells
+
+  gathered = util.gather((len(array), (name, bigendian(array))) for name, array in kwargs.items())
+
+  invalid_length = [name for n, arrays in gathered if n not in (len(points), len(cells)) for name, array in arrays]
+  if invalid_length:
+    raise Exception('data length matches neither points nor cells: {}'.format(', '.join(invalid_length)))
+
+  invalid_dimension = [name for n, arrays in gathered for name, array in arrays if array.ndim not in vtkndim or any(n>3 for n in array.shape[1:])]
+  if invalid_dimension:
+    raise Exception('invalid array dimension: {}'.format(', '.join(invalid_dimension)))
+
+  invalid_dtype = [name for n, arrays in gathered for name, array in arrays if array.dtype not in vtkdtype]
+  if invalid_dtype:
+    raise Exception('invalid array data type: {}'.format(', '.join(invalid_dtype)))
+
+  name_vtk = name + '.vtk'
+  with log.context(name_vtk), core.open_in_outdir(name_vtk, 'wb') as vtk:
+    vtk.write(b'# vtk DataFile Version 3.0\nvtk output\nBINARY\nDATASET UNSTRUCTURED_GRID\n')
+    vtk.write('POINTS {} {}\n'.format(len(points), vtkdtype[points.dtype]).encode('ascii'))
+    points.tofile(vtk)
+    vtk.write('CELLS {} {}\n'.format(len(t_cells), t_cells.size).encode('ascii'))
+    t_cells.tofile(vtk)
+    vtk.write('CELL_TYPES {}\n'.format(len(cells)).encode('ascii'))
+    numpy.array(celltype, dtype='>u4').repeat(len(cells)).tofile(vtk)
+    for n, arrays in gathered:
+      vtk.write('{}_DATA {}\n'.format('POINT' if n == len(points) else 'CELL', n).encode('ascii'))
+      for name, array in arrays:
+        vtk.write(vtkndim[array.ndim].format(name, vtkdtype[array.dtype]).encode('ascii'))
+        numpy.pad(array, [[0,0]] + [[0,3-n] for n in array.shape[1:]], mode='constant').tofile(vtk)
+
+  log.user(name_vtk)
 
 # vim:sw=2:sts=2:et
