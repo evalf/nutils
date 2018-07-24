224011622b66a073f24a7722789693a7a51b2bef
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2018-07-24 15:51:05 +0530

    update adaptivity example

--- examples/adaptivity.py
+++ examples/adaptivity.py
@@ -1,6 +1,24 @@
 #! /usr/bin/env python3
+#
+# In this script we solve the Laplace problem on a unit square that has the
+# bottom-right quadrant removed (a.k.a. an L-shaped domain) with Dirichlet
+# boundary conditions matching the harmonic function
+#
+# .. math:: \sqrt[3]{x^2 + y^2} \cos\left(\tfrac23 \arctan\frac{y+x}{y-x}\right),
+#
+# shifted by 0.5 such that the origin coincides with the middle of the unit
+# square. This variation of a well known benchmark problem is known to converge
+# suboptimally under uniform refinement due to a singular gradient in the
+# reentrant corner. This script demonstrates that optimal convergence can be
+# restored by using adaptive refinement.
 
 import nutils, numpy
+
+# The main function defines the parameter space for the script. Configurable
+# parameters are the element type (square, triangle, or mixed), type of basis
+# function (std or spline, with availability depending on element type),
+# polynomial degree, and the number of refinement steps to perform before
+# quitting (by default the script will run forever).
 
 def main(etype: 'type of elements (square/triangle/mixed)' = 'square',
          btype: 'type of basis function (h/th-std/spline)' = 'h-std',
@@ -9,7 +27,7 @@ def main(etype: 'type of elements (square/triangle/mixed)' = 'square',
 
   domain, geom = nutils.mesh.unitsquare(2, etype)
 
-  x, y = geom * 2 - 1
+  x, y = geom - .5
   exact = (x**2 + y**2)**(1/3) * nutils.function.cos(nutils.function.arctan2(y+x, y-x) * (2/3))
   domain = domain.trim(exact-1e-15, maxrefine=0)
   linreg = nutils.util.linear_regressor()
@@ -54,8 +72,20 @@ def main(etype: 'type of elements (square/triangle/mixed)' = 'square',
 
   return ndofs, error, rate, lhs
 
+# If the script is executed (as opposed to imported), :func:`nutils.cli.run`
+# calls the main function with arguments provided from the command line. For
+# example, to perform four refinement steps with quadratic basis functions
+# starting from a triangle mesh run :sh:`python3 adaptivity.py etype=triangle
+# degree=2 nrefine=4`.
+
 if __name__ == '__main__':
   nutils.cli.run(main)
+
+# Once a simulation is developed and tested, it is good practice to save a few
+# strategicly chosen return values for routine regression testing. Here we use
+# the standard :mod:`unittest` framework, with
+# :func:`nutils.numeric.assert_allclose64` facilitating the embedding of
+# desired results as compressed base64 data.
 
 import unittest
 
@@ -64,29 +94,29 @@ class test(unittest.TestCase):
   def test_square_quadratic(self):
     ndofs, error, rate, lhs = main(nrefine=2, etype='square', degree=2)
     self.assertEqual(ndofs, 149)
-    numpy.testing.assert_almost_equal(error, [0.00104, 0.05495], decimal=5)
+    numpy.testing.assert_almost_equal(error, [0.00065, 0.03461], decimal=5)
     numpy.testing.assert_almost_equal(rate, [-1.066, -0.478], decimal=3)
-    nutils.numeric.assert_allclose64(lhs, 'eNo1jyFrQnEUxd/qTH4FeciEFf3fc/7NNBwsOI'
-      'wmMSz5wGA2zaDFtPT8Bi8Iq6bBZJuCRdC4Z1OEgdgeGHbCdtM9F845v9vhkK+MfOw//BE5Vnnr'
-      'n6RC1DDAt1Tkty5zdQv+pm4DVOUJgszV5Bhq27oQR3TYR5NnXvuCj5niASW+M2XGHmfI4QcjJl'
-      'zwjmPMLcE9u3xhni207REnFNjgGjc4uJ09Y4kr7vEl3cIYM6QIXdE2ZpjgDXuskVdSjzFX5Usl'
-      'clP7tIk8DWUu1Fnw/8RTeZZK76o9FV0QRG6jhpMIRiI864NLpWg7USSiLLGpy6ocuoNr29xy+q'
-      'SPX7TTY6M=')
+    nutils.numeric.assert_allclose64(lhs, 'eNo1j6FrQmEUxT8RBi4KllVfMsl3z/nK4zEmLC'
+      '6bhsKCw2gSw5IPFsymGbZiWnr+By8Ii7Yhsk3BMtC4Z9sJ223ncs85vzvmM9+Yhix8hDIjtnkd'
+      'HqQSdDDDj1Qajr5qPXN/07MZ2vI4V7UOIvmdO/oEZY45xYDnoR7ikLHAHVpcs2A1TLhChDO+MO'
+      'eWt5xjYzm6fOQrGxxiZPeoMGaf37hCyU72hB0u6PglPcQcKxRI/KUd7AYLvMPpsqGkCTPumzWf'
+      '+qV92kKevjK36ozDP/FSnh1iteWiqWuf+oMaKuyKaC1i52rKPokiF2WLA/20bya+ZCPbWKRPpv'
+      'gFaedebw==')
 
   def test_triangle_quadratic(self):
     ndofs, error, rate, lhs = main(nrefine=2, etype='triangle', degree=2)
     self.assertEqual(ndofs, 98)
-    numpy.testing.assert_almost_equal(error, [0.00219, 0.08451], decimal=5)
+    numpy.testing.assert_almost_equal(error, [0.00138, 0.05324], decimal=5)
     numpy.testing.assert_almost_equal(rate, [-1.111, -0.548], decimal=3)
-    nutils.numeric.assert_allclose64(lhs, 'eNoljS0Og1AQhB9JazhGg0S9/QsGWw8hyAaBaM'
-      'BxAEQNZ+AWNRhMRdMDIBpMTWUtBtl9sJMVs5mdr+KO7/yjIwc0sy8x13yjM71hAmMGOnGKRueK'
-      'kdTy5VA+3MtLLtKrD/USiS8tPzXZsjF7diDXkuIEM8fa7/oCZXRKqjghDzPdA+a0QGEFC+v+St'
-      'tsrBWcRnhAg4IjLJvPKaMVEtoZpS1UB/RUf+Q6Og8=')
+    nutils.numeric.assert_allclose64(lhs, 'eNprMV1oesqU2VTO1Nbko6myWbhpq+kckwST90'
+      'avjRgYzptYm+YYMwBBk3GQWavZb1NXs2+mm83um1WYbQbyXYEiQWbKZjNM7wJVzjBlYICoPW8C'
+      'MiXH+LXRR9NwoPkg82xN5IB2MZu2mGabSBnnAbGscYEJj3GVYQAQg/TVGfaA7RI0BsErRjeNeo'
+      'wDgDQPmF9gkmciaJxtArGjzrAKCGWNpYAQAL0kOBE=')
 
   def test_mixed_linear(self):
     ndofs, error, rate, lhs = main(nrefine=2, etype='mixed', degree=1)
     self.assertEqual(ndofs, 34)
-    numpy.testing.assert_almost_equal(error, [0.00714, 0.18546], decimal=5)
+    numpy.testing.assert_almost_equal(error, [0.00450, 0.11683], decimal=5)
     numpy.testing.assert_almost_equal(rate, [-1.143, -0.545], decimal=3)
-    nutils.numeric.assert_allclose64(lhs, 'eNrLNltnpmlabcphLm7GAATZ5vfMjppnm8P41a'
-      'Yg+Wyzlbo7DUH8yCs+F3wuRF6JuXTx3E7DmEsMDCt1AXoBFaA=')
+    nutils.numeric.assert_allclose64(lhs, 'eNprMT1u6mQyxUTRzMCUAQhazL6b3jNrMYPxp5'
+      'iA5FtMD+lcMgDxHa4aXzS+6HDV+fKO85cMnC8zMBzSAQDBThbY')
