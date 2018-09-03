3bc4dff582b9139beca3145e557b1c3ce89b6c9e
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2018-09-03 17:32:51 +0200

    allow relax>1 in minimize in case of diagonal shift

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -139,12 +139,12 @@ class finitestrain(TestCase):
     energy = domain.integral(((strain**2).sum([0,1]) + 150*(function.determinant(Geom.grad(geom))-1)**2)*function.J(geom), degree=6)
     nshift = 0
     for iiter, (lhs, info) in enumerate(solver.minimize('dofs', energy, constrain=cons)):
-      self.assertLess(iiter, 137)
+      self.assertLess(iiter, 73)
       if info.shift:
         nshift += 1
       if info.resnorm < self.tol:
         break
-    self.assertEqual(nshift, 108)
+    self.assertEqual(nshift, 43)
 
 
 @parametrize
