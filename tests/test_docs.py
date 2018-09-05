fba8169cb77dc8e5560be4210708b18514959df3
Joost van Zwieten <joostvanzwieten@evalf.nl>
2018-09-05 16:14:11 +0200

    skip doctest if not all requirements are satisfied

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,12 +1,28 @@
-import doctest as _doctest, importlib, os, tempfile, pathlib, functools, warnings, subprocess, sys, treelog
-from nutils.testing import *
+import doctest as _doctest, unittest, importlib, os, tempfile, pathlib, functools, warnings, subprocess, sys, treelog
+import nutils.testing
 
 _doctestlog = treelog.FilterLog(treelog.StdoutLog(), minlevel=1)
 
-@parametrize
-class module(TestCase):
+class DocTestCase(nutils.testing.ContextTestCase, _doctest.DocTestCase):
+
+  def __init__(self, test, *, requires=None, **kwargs):
+    self.__test = test
+    self.__requires = tuple(requires) if requires else ()
+    super().__init__(test, **kwargs)
 
   def setUpContext(self, stack):
+    lines = self.__test.docstring.splitlines()
+    indent = min((len(line) - len(line.lstrip()) for line in lines[1:] if line.strip()), default=0)
+    blank = True
+    requires = list(self.__requires)
+    for line in lines:
+      if blank and line[indent:].startswith('.. requires:: '):
+        requires.extend(name.strip() for name in line[indent+13:].split(','))
+      blank = not line.strip()
+    missing = tuple(filter(nutils.testing._not_has_module, requires))
+    if missing:
+      self.skipTest('missing module{}: {}'.format('s' if len(missing) > 1 else '', ','.join(missing)))
+
     super().setUpContext(stack)
     stack.enter_context(warnings.catch_warnings())
     warnings.simplefilter('ignore')
@@ -17,43 +33,46 @@ class module(TestCase):
       stack.callback(numpy.set_printoptions, **printoptions)
       numpy.set_printoptions(legacy='1.13')
 
-  def test(self):
-    failcnt, testcnt = _doctest.testmod(importlib.import_module(self.name), optionflags=_doctest.ELLIPSIS)
-    self.assertEqual(failcnt, 0)
+  def shortDescription(self):
+    return None
 
-@parametrize
-class file(TestCase):
+  def __repr__(self):
+    return '{} ({}.doctest)'.format(self.id(), __name__)
 
-  def setUpContext(self, stack):
-    super().setUpContext(stack)
-    stack.enter_context(warnings.catch_warnings())
-    warnings.simplefilter('ignore')
-    stack.enter_context(treelog.set(_doctestlog))
+  __str__ = __repr__
 
-  def test(self):
-    failcnt, testcnt = _doctest.testfile(str(self.path), module_relative=False, optionflags=_doctest.ELLIPSIS)
-    self.assertEqual(failcnt, 0)
-
+doctest = unittest.TestSuite()
+parser = _doctest.DocTestParser()
+finder = _doctest.DocTestFinder(parser=parser)
+checker = nutils.testing.FloatNeighborhoodOutputChecker()
 root = pathlib.Path(__file__).parent.parent
-for path in sorted((root / 'nutils').glob('**/*.py')):
+for path in sorted((root/'nutils').glob('**/*.py')):
   name = '.'.join(path.relative_to(root).parts)[:-3]
   if name.endswith('.__init__'):
     name = name[:-9]
-  module(name.replace('.', '/'), name=name)
-
-for path in sorted((root / 'docs').glob('**/*.rst')):
-  if path == root / 'docs' / 'tutorial.rst':
-    continue
+  module = importlib.import_module(name)
+  for test in sorted(finder.find(module)):
+    if len(test.examples) == 0:
+      continue
+    if not test.filename:
+      test.filename = module.__file__
+    doctest.addTest(DocTestCase(test, optionflags=_doctest.ELLIPSIS, checker=checker))
+for path in sorted((root/'docs').glob('**/*.rst')):
   name = str(path.relative_to(root))
-  file(name[:-4], name=name, path=path)
+  with path.open() as f:
+    doc = f.read()
+  test = parser.get_doctest(doc, globs={}, name=name, filename=str(path), lineno=0)
+  if test.examples:
+    doctest.addTest(DocTestCase(test, optionflags=_doctest.ELLIPSIS, checker=checker, requires=['matplotlib']))
 
 
-class sphinx(TestCase):
+class sphinx(nutils.testing.TestCase):
 
   def setUpContext(self, stack):
     super().setUpContext(stack)
     self.tmpdir = pathlib.Path(stack.enter_context(tempfile.TemporaryDirectory(prefix='nutils')))
 
+  @nutils.testing.requires('sphinx', 'matplotlib', 'scipy')
   def test(self):
     from sphinx.application import Sphinx
     app = Sphinx(srcdir=str(root/'docs'),
@@ -67,3 +86,11 @@ class sphinx(TestCase):
     app.build()
     if app.statuscode:
       self.fail('sphinx build failed with code {}'.format(app.statuscode))
+
+
+def load_tests(loader, suite, pattern):
+  # Ignore default suite (containing `DocTestCase`).
+  suite = unittest.TestSuite()
+  suite.addTest(doctest)
+  suite.addTests(loader.loadTestsFromTestCase(sphinx))
+  return suite
