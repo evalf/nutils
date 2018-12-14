689f08dc9d189fe6104337a935c67b748cc5bf0b
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2018-12-14 15:04:55 +0100

    treat removedofs in stdbasis equal to splinebasis

--- nutils/topology.py
+++ nutils/topology.py
@@ -1476,7 +1476,7 @@ class StructuredTopology(Topology):
     if numeric.isint(degree):
       degree = (degree,) * self.ndims
 
-    if removedofs == None:
+    if removedofs is None or isinstance(removedofs[0], int):
       removedofs = [None] * self.ndims
     else:
       assert len(removedofs) == self.ndims
