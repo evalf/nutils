27ed6873b7f1b575abec6ccd38f977bc1baa53b0
Joost van Zwieten <joostvanzwieten@evalf.com>
2019-07-02 15:59:49 +0200

    drop deprecated version of function.elemwise

--- nutils/function.py
+++ nutils/function.py
@@ -3965,24 +3965,8 @@ def eig(arg, axes=(-2,-1), symmetric=False):
   eigval, eigvec = Eig(transposed, symmetric)
   return Tuple([transpose(diagonalize(eigval), _invtrans(trans)), transpose(eigvec, _invtrans(trans))])
 
-def elemwise(*args, **kwargs):
-  if 'fmap' in kwargs or len(args) >= 1 and isinstance(args[0], dict):
-    warnings.deprecation('passing a dictionary with transforms and values to elemwise is deprecated, pass the transforms and values as separate arguments instead')
-    return _deprecated_elemwise(*args, **kwargs)
-  else:
-    return _new_elemwise(*args, **kwargs)
-
 @types.apply_annotations
-def _new_elemwise(transforms:transformseq.stricttransforms, values:types.tuple[types.frozenarray]):
-  index, tail = TransformsIndexWithTail(transforms, TRANS)
-  return Elemwise(values, index, dtype=float)
-
-def _deprecated_elemwise(fmap, shape, default=None):
-  if default is not None:
-    raise NotImplemented('default is not supported anymore')
-  transforms = tuple(sorted(fmap, key=lambda trans: tuple(map(id, trans))))
-  values = tuple(fmap[trans] for trans in transforms)
-  transforms = transformseq.PlainTransforms(transforms, transforms[0][-1].fromdims)
+def elemwise(transforms:transformseq.stricttransforms, values:types.tuple[types.frozenarray]):
   index, tail = TransformsIndexWithTail(transforms, TRANS)
   return Elemwise(values, index, dtype=float)
 
