ed8d16ffa2a5147374eb10a14a9dd53d34938d72
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2020-01-16 15:40:58 +0100

    bump version to 5.2

--- nutils/__init__.py
+++ nutils/__init__.py
@@ -24,7 +24,7 @@ from distutils.version import LooseVersion
 assert sys.version_info >= (3, 5)
 assert LooseVersion(numpy.version.version) >= LooseVersion('1.8'), 'nutils requires numpy 1.8 or higher, got {}'.format(numpy.version.version)
 
-version = '5.1'
+version = '5.2'
 version_name = 'farfalle'
 long_version = ('{} "{}"' if version_name else '{}').format(version, version_name)
 
