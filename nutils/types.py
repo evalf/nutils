2b376b5a91467a5d7866f79a0e01c1e19478eed0
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2020-04-06 18:04:34 +0200

    remove frozenset automethods

--- nutils/types.py
+++ nutils/types.py
@@ -1013,10 +1013,6 @@ class frozenmultiset(collections.abc.Container, metaclass=_frozenmultisetmeta):
 
   >>> frozenmultiset(['spam', 'spam', 'eggs']) == frozenmultiset(['spam', 'eggs'])
   False
-
-  .. automethod:: __and__
-  .. automethod:: __add__
-  .. automethod:: __sub__
   '''
 
   __slots__ = '__items', '__key'
