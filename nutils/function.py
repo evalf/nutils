5dfba7ef3c7b55b6f89cedb0bd55ce2e6c9905e7
Joost van Zwieten <joostvanzwieten@evalf.com>
2020-04-14 21:36:08 +0200

    fix subsamplemeta with points is None

--- nutils/function.py
+++ nutils/function.py
@@ -1356,7 +1356,7 @@ class ApplyTransforms(Array):
       slices = {}
       isubsamples = {}
       for isubsample, subsample in enumerate(subsamples):
-        if not self.roots.isdisjoint(subsample.roots) and subsample.points is None:
+        if not self.roots.isdisjoint(subsample.roots) or subsample.points is None:
           return ApplyTransforms(tail, self._linear)
 
         from0 = 0
