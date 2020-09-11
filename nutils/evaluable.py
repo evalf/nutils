213ce9b32fd49f850d3e35511437a2b9063851da
Joost van Zwieten <joostvanzwieten@evalf.com>
2020-09-11 13:13:19 +0200

    remove evaluable.partition

--- nutils/evaluable.py
+++ nutils/evaluable.py
@@ -3079,44 +3079,6 @@ def tanh(arg):
 def arctanh(arg):
   return .5 * (ln(1+arg) - ln(1-arg))
 
-def partition(f, *levels):
-  '''Create a partition of unity for a scalar function f.
-
-  When ``n`` levels are specified, ``n+1`` indicator functions are formed that
-  evaluate to one if and only if the following condition holds::
-
-      indicator 0: f < levels[0]
-      indicator 1: levels[0] < f < levels[1]
-      ...
-      indicator n-1: levels[n-2] < f < levels[n-1]
-      indicator n: f > levels[n-1]
-
-  At the interval boundaries the indicators evaluate to one half, in the
-  remainder of the domain they evaluate to zero such that the whole forms a
-  partition of unity. The partitions can be used to create a piecewise
-  continuous function by means of multiplication and addition.
-
-  The following example creates a topology consiting of three elements, and a
-  function ``f`` that is zero in the first element, parabolic in the second,
-  and zero again in the third element.
-
-  Args
-  ----
-  f : :class:`Array`
-      Scalar-valued function
-  levels : scalar constants or :class:`Array`\\s
-      The interval endpoints.
-
-  Returns
-  -------
-  :class:`list` of scalar :class:`Array`\\s
-      The indicator functions.
-  '''
-
-  signs = [Sign(f - level) for level in levels]
-  steps = map(subtract, signs[:-1], signs[1:])
-  return [.5 - .5 * signs[0]] + [.5 * step for step in steps] + [.5 + .5 * signs[-1]]
-
 def trace(arg, n1=-2, n2=-1):
   return sum(_takediag(arg, n1, n2), -1)
 
