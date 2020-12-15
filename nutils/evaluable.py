evalf/nutils

commit f60c61d0ebdbc58019143085fc7855df6d1d5517

fix Inflate simplifications for duplicate indices
It is currently allowed, however extremely unlikely to occur in practice, to
pass an non-unique array of indices to `evaluable.Inflate`. Several
simplifications were written assuming uniqueness. This commit fixes these
simplifications and adds unittests.

nutils/evaluable.py
@@ -2094,39 +2094,6 @@ class Eig(Evaluable):
   def evalf(self, arr):
     return (numpy.linalg.eigh if self.symmetric else numpy.linalg.eig)(arr)
 
-class Intersect(Evaluable):
-
-  def __init__(self, index1, index2):
-    self.index1 = index1
-    self.index2 = index2
-    super().__init__(args=[index1, index2])
-
-  def evalf(self, index1, index2):
-    ind, subind1, subind2 = numpy.intersect1d(index1, index2, return_indices=True)
-    return ind, subind1, subind2, numpy.array(len(ind))
-
-  def __len__(self):
-    return 3
-
-  def __iter__(self):
-    if self.index1 == self.index2:
-      return iter([self.index1, Range(self.index1.shape[0]), Range(self.index1.shape[0])])
-    if all(isinstance(index, (Range, InRange)) and index.length.isconstant and index.offset.isconstant for index in (self.index1, self.index2)):
-      length1 = self.index1.length.eval()
-      offset1 = self.index1.offset.eval()
-      length2 = self.index2.length.eval()
-      offset2 = self.index2.offset.eval()
-      offset = max(offset1, offset2)
-      length = min(offset1 + length1, offset2 + length2) - offset
-      if length <= 0:
-        return (Zeros([0], dtype=int) for i in range(3))
-      if all(isinstance(index, Range) for index in (self.index1, self.index2)):
-        return (Range(length, offset-o) for o in (0, offset1, offset2))
-    if self.isconstant:
-      return map(Constant, self.eval()[:3])
-    shape = ArrayFromTuple(self, 3, shape=(), dtype=int),
-    return (ArrayFromTuple(self, i, shape, dtype=int) for i in range(3))
-
 class ArrayFromTuple(Array):
 
   __slots__ = 'arrays', 'index'
@@ -2272,13 +2239,16 @@ class Inflate(Array):
   def _take(self, index, axis):
     if axis != self.ndim-1:
       return Inflate(_take(self.func, index, axis), self.dofmap, self.length)
-    if index == self.dofmap:
+    if index == self.dofmap and (self.dofmap.size == 1 or self.dofmap.isconstant and _isunique(self.dofmap.eval().ravel())):
       return self.func
-    ind, subind1, subind2 = Intersect(self.dofmap, index)
-    if ind.shape[0] == 0:
-      return Zeros(self.shape[:axis] + index.shape + self.shape[axis+1:], dtype=self.dtype)
-    if self.dofmap.ndim == 1 and index.ndim == 1:
-      return Inflate(_take(self.func, subind1, axis), subind2, index.shape[0])
+    if self.dofmap.isconstant and index.isconstant:
+      ind1 = self.dofmap.eval()
+      ind2 = index.eval()
+      ind, subind1, subind2 = numpy.intersect1d(ind1, ind2, return_indices=True)
+      if ind.size == 0:
+        return Zeros(self.shape[:-1] + index.shape, dtype=self.dtype)
+      elif self.dofmap.ndim == index.ndim == 1 and _isunique(ind1) and _isunique(ind2):
+        return Inflate(Take(self.func, subind1), subind2, index.shape[0])
 
   def _diagonalize(self, axis):
     if axis != self.ndim-1:
@@ -2297,7 +2267,8 @@ class Inflate(Array):
       return Inflate(unravel(self.func, axis, shape), self.dofmap, self.length)
 
   def _sign(self):
-    return Inflate(Sign(self.func), self.dofmap, self.length)
+    if self.dofmap.isconstant and _isunique(self.dofmap.eval()):
+      return Inflate(Sign(self.func), self.dofmap, self.length)
 
 class Diagonalize(Array):
 
@@ -2930,6 +2901,9 @@ def _inflate_scalar(arg, shape):
   for idim, length in enumerate(shape):
     arg = insertaxis(arg, idim, length)
   return arg
+
+def _isunique(array):
+  return numpy.unique(array).size == array.size
 
 # FUNCTIONS
 

tests/test_evaluable.py
@@ -132,6 +132,14 @@ class check(TestCase):
           desired=numpy.take(self.n_op_argsfun, indices, axis=iax),
           actual=evaluable.take(self.op_args, indices, axis=iax))
 
+  def test_take_duplicate_indices(self):
+    for iax, sh in enumerate(self.op_args.shape):
+      if sh >= 2:
+        indices = [0,sh-1,0,0]
+        self.assertFunctionAlmostEqual(decimal=15,
+          desired=numpy.take(self.n_op_argsfun, indices, axis=iax),
+          actual=evaluable.take(self.op_args, evaluable.Guard(evaluable.asarray(indices)), axis=iax))
+
   def test_inflate(self):
     for iax, sh in enumerate(self.op_args.shape):
       dofmap = evaluable.Constant(numpy.arange(sh) * 2)
@@ -140,6 +148,15 @@ class check(TestCase):
       self.assertFunctionAlmostEqual(decimal=15,
         desired=desired,
         actual=evaluable._inflate(self.op_args, dofmap=dofmap, length=sh*2-1, axis=iax))
+
+  def test_inflate_duplicate_indices(self):
+    for iax, sh in enumerate(self.op_args.shape):
+      dofmap = numpy.arange(sh) % 2
+      desired = numpy.zeros(self.n_op_argsfun.shape[:iax] + (2,) + self.n_op_argsfun.shape[iax+1:], dtype=self.n_op_argsfun.dtype)
+      numpy.add.at(desired, (slice(None),)*iax+(dofmap,), self.n_op_argsfun)
+      self.assertFunctionAlmostEqual(decimal=15,
+        desired=desired,
+        actual=evaluable._inflate(self.op_args, dofmap=dofmap, length=2, axis=iax))
 
   def test_diagonalize(self):
     for axis in range(self.op_args.ndim):
@@ -246,7 +263,7 @@ class check(TestCase):
         actual = numpy.zeros_like(self.n_op_argsfun)
         for ind, f in op_args._desparsify(axis):
           _ind = ind.eval(**evalargs)
-          actual[(slice(None),)*(axis)+(_ind,)] += f.eval(**evalargs)
+          numpy.add.at(actual, (slice(None),)*(axis)+(_ind,), f.eval(**evalargs))
         self.assertArrayAlmostEqual(actual, self.n_op_argsfun, decimal=15)
 
   @parametrize.enable_if(lambda hasgrad, **kwargs: hasgrad)
@@ -342,6 +359,9 @@ _check('ravel', lambda f: evaluable.ravel(f,axis=1), lambda a: a.reshape(4,4,4,4
 _check('unravel', lambda f: evaluable.unravel(f,axis=1,shape=[2,2]), lambda a: a.reshape(4,2,2,4,4), [(4,4,4,4)])
 _check('inflate', lambda f: evaluable._inflate(f,dofmap=evaluable.Guard([0,3]),length=4,axis=1), lambda a: numpy.concatenate([a[:,:1], numpy.zeros_like(a), a[:,1:]], axis=1), [(4,2,4)])
 _check('inflate-constant', lambda f: evaluable._inflate(f,dofmap=[0,3],length=4,axis=1), lambda a: numpy.concatenate([a[:,:1], numpy.zeros_like(a), a[:,1:]], axis=1), [(4,2,4)])
+_check('inflate-duplicate', lambda f: evaluable.Inflate(f,dofmap=[0,1,0,3],length=4), lambda a: numpy.stack([a[:,0]+a[:,2], a[:,1], numpy.zeros_like(a[:,0]), a[:,3]], axis=1), [(2,4)])
+_check('take', lambda f: evaluable.Take(f, [0,3,2]), lambda a: a[:,[0,3,2]], [(2,4)])
+_check('take-duplicate', lambda f: evaluable.Take(f, [0,3,0]), lambda a: a[:,[0,3,0]], [(2,4)])
 _check('choose', lambda a, b, c: evaluable.Choose(evaluable.appendaxes(evaluable.Int(a)%2, (3,3)), [b,c]), lambda a, b, c: numpy.choose(a[_,_].astype(int)%2, [b,c]), [(), (3,3), (3,3)])
 _check('slice', lambda a: evaluable.asarray(a)[::2], lambda a: a[::2], [(5,3)])
 
@@ -381,11 +401,6 @@ class blocks(TestCase):
     blocks = evaluable.multiply(evaluable._inflate([1,2], dofmap=[0,2], length=4, axis=0), evaluable._inflate([3,4], dofmap=[1,3], length=4, axis=0)).blocks
     self.assertEqual(blocks, ())
 
-  def test_multiply_overlap(self):
-    ((i,), f), = evaluable.multiply(evaluable._inflate([1,2], dofmap=evaluable.Guard([0,1]), length=3, axis=0), evaluable._inflate([3,4], dofmap=evaluable.Guard([1,2]), length=3, axis=0)).blocks
-    self.assertAllEqual(i.eval(), [1])
-    self.assertAllEqual(f.eval(), [2*3])
-
   def test_takediag(self):
     ((i,), f), = evaluable.takediag([[1,2,3],[4,5,6],[7,8,9]]).blocks
     self.assertAllEqual(i.eval(), [0,1,2])
@@ -409,11 +424,6 @@ class blocks(TestCase):
   def test_takediag_disjoint(self):
     blocks = evaluable.takediag(evaluable._inflate(evaluable._inflate([[1,2],[3,4]], dofmap=[0,2], length=4, axis=0), dofmap=[1,3], length=4, axis=1)).blocks
     self.assertEqual(blocks, ())
-
-  def test_takediag_overlap(self):
-    ((i,), f), = evaluable.takediag(evaluable._inflate(evaluable._inflate([[1,2],[3,4]], dofmap=evaluable.Guard([0,1]), length=3, axis=0), dofmap=evaluable.Guard([1,2]), length=3, axis=1)).blocks
-    self.assertAllEqual(i.eval(), [1])
-    self.assertAllEqual(f.eval(), [3])
 
 
 class commutativity(TestCase):

commit 10c52eedb29acfa4e4d3696b8fe462f686027a21

add SwapInflateTake
Commit f60c61d0 introduced a potential regression by removing the inflate/take
simplification for non-constant indices. This patch reintroduces these
simplifications by introducing SwapInflateTake, which generates the required
(potentially duplicate) indices at the time of evaluation.

nutils/evaluable.py
@@ -2489,7 +2489,7 @@ class Inflate(Array):
   def evalf(self, array, indices, length):
     assert indices.ndim == self.dofmap.ndim
     assert length.ndim == 0
-    if self.warn:
+    if self.warn and int(length) > len(indices):
       warnings.warn('using explicit inflation; this is usually a bug.', ExpensiveEvaluationWarning)
     inflated = numpy.zeros(array.shape[:array.ndim-indices.ndim] + (length,), dtype=self.dtype)
     numpy.add.at(inflated, (slice(None),)*(self.ndim-1)+(indices,), array)
@@ -2531,16 +2531,21 @@ class Inflate(Array):
   def _take(self, index, axis):
     if axis != self.ndim-1:
       return Inflate(_take(self.func, index, axis), self.dofmap, self.length)
-    if index == self.dofmap and (self.dofmap.size == 1 or self.dofmap.isconstant and _isunique(self.dofmap.eval().ravel())):
-      return self.func
-    if self.dofmap.isconstant and index.isconstant:
-      ind1 = self.dofmap.eval()
-      ind2 = index.eval()
-      ind, subind1, subind2 = numpy.intersect1d(ind1, ind2, return_indices=True)
-      if ind.size == 0:
-        return Zeros(self.shape[:-1] + index.shape, dtype=self.dtype)
-      elif self.dofmap.ndim == index.ndim == 1 and _isunique(ind1) and _isunique(ind2):
-        return Inflate(Take(self.func, subind1), subind2, index.shape[0])
+    newindex, newdofmap = SwapInflateTake(self.dofmap, index)
+    if self.dofmap.ndim:
+      func = self.func
+      for i in range(self.dofmap.ndim-1):
+        func = Ravel(func)
+      intersection = Take(func, newindex)
+    else: # kronecker; newindex is all zeros (but of varying length)
+      intersection = InsertAxis(self.func, newindex.shape[0])
+    if index.ndim:
+      swapped = Inflate(intersection, newdofmap, index.size)
+      for i in range(index.ndim-1):
+        swapped = Unravel(swapped, index.shape[i], util.product(index.shape[i+1:]))
+    else: # get; newdofmap is all zeros (but of varying length)
+      swapped = Sum(intersection)
+    return swapped
 
   def _diagonalize(self, axis):
     if axis != self.ndim-1:
@@ -2575,6 +2580,45 @@ class Inflate(Array):
         inflate_indices = appendaxes(self.dofmap, values.shape)
       chunks.append((*indices[:keep_dim], inflate_indices, values))
     return tuple(chunks)
+
+class SwapInflateTake(Evaluable):
+
+  def __init__(self, inflateidx, takeidx):
+    self.inflateidx = inflateidx
+    self.takeidx = takeidx
+    super().__init__(args=[inflateidx, takeidx])
+
+  def __iter__(self):
+    shape = ArrayFromTuple(self, index=2, shape=(), dtype=int),
+    return (ArrayFromTuple(self, index=index, shape=shape, dtype=int) for index in range(2))
+
+  def evalf(self, inflateidx, takeidx):
+    uniqueinflate = _isunique(inflateidx)
+    uniquetake = _isunique(takeidx)
+    unique = uniqueinflate and uniquetake
+    # If both indices are unique (i.e. they do not contain duplicates) then the
+    # take and inflate operations can simply be restricted to the intersection,
+    # with the the location of the intersection in the original index vectors
+    # being the new indices for the swapped operations.
+    intersection, subinflate, subtake = numpy.intersect1d(inflateidx, takeidx, return_indices=True, assume_unique=unique)
+    if unique:
+      return subinflate, subtake, numpy.array(len(intersection))
+    # Otherwise, while still limiting the operations to the intersection, we
+    # need to add the appropriate duplications on either side. The easiest way
+    # to do this is to form the permutation matrix A for take (may contain
+    # multiple items per column) and B for inflate (may contain several items
+    # per row) and take the product AB for the combined operation. To then
+    # decompose AB into the equivalent take followed by inflate we can simply
+    # take the two index vectors from AB.nonzero() and form CD = AB. The
+    # algorithm below does precisely this without forming AB explicitly.
+    newinflate = []
+    newtake = []
+    for k, n in enumerate(intersection):
+      for i in [subtake[k]] if uniquetake else numpy.equal(takeidx.ravel(), n).nonzero()[0]:
+        for j in [subinflate[k]] if uniqueinflate else numpy.equal(inflateidx.ravel(), n).nonzero()[0]:
+          newinflate.append(i)
+          newtake.append(j)
+    return numpy.array(newtake), numpy.array(newinflate), numpy.array(len(newtake))
 
 class Diagonalize(Array):
 