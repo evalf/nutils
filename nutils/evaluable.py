b36fdc763ed494ed2100cffb7b1bbac3680a6be8
Joost van Zwieten <joostvanzwieten@evalf.com>
2021-01-25 16:29:33 +0100

    fix Inflate explicit infl. check with not 1d dofs
    The `Inflate` evaluable warns about expensive evaluation if the dofmap is
    smaller than the length of the inflated axis. The check incorrectly assumes the
    dofmap is 1d, while `Inflate` actually accepts a dofmap of any dimension. This
    patch fixes the problem by looking at the size of the dofmap instead of the
    length of the first axis and adds a unit test with a scalar dofmap.

--- nutils/evaluable.py
+++ nutils/evaluable.py
@@ -2515,7 +2515,7 @@ class Inflate(Array):
   def evalf(self, array, indices, length):
     assert indices.ndim == self.dofmap.ndim
     assert length.ndim == 0
-    if self.warn and int(length) > len(indices):
+    if self.warn and int(length) > indices.size:
       warnings.warn('using explicit inflation; this is usually a bug.', ExpensiveEvaluationWarning)
     inflated = numpy.zeros(array.shape[:array.ndim-indices.ndim] + (length,), dtype=self.dtype)
     numpy.add.at(inflated, (slice(None),)*(self.ndim-1)+(indices,), array)
