2dc082c95ecca42b8a3c9ae653d8321138b8dfaa
Joost van Zwieten <joostvanzwieten@evalf.com>
2021-07-09 15:23:35 +0200

    add Sample.empty,__add__,__mul__,take
    This patch adds transforms-sequences-less, multi-space implementations of
    `Sample`, which are going to be used by corresponding multi-space topologies.

--- nutils/sample.py
+++ nutils/sample.py
@@ -97,6 +97,10 @@ class Sample(types.Singleton):
       sample = _CustomIndex(sample, types.arraydata(index))
     return sample
 
+  @staticmethod
+  def empty(spaces: Tuple[str, ...], ndims: int) -> 'Sample':
+    return _Empty(spaces, ndims)
+
   def __init__(self, spaces: Tuple[str, ...], ndims: int, nelems: int, npoints: int) -> None:
     '''
     parameters
@@ -116,6 +120,26 @@ class Sample(types.Singleton):
     self.nelems = nelems
     self.npoints = npoints
 
+  def __add__(self, other: 'Sample') -> 'Sample':
+    if not isinstance(other, Sample):
+      return NotImplemented
+    elif self.spaces != other.spaces:
+      raise ValueError('Cannot add samples with different spaces.')
+    elif other.npoints == 0:
+      return self
+    elif self.npoints == 0:
+      return other
+    else:
+      return _Add(self, other)
+
+  def __mul__(self, other: 'Sample') -> 'Sample':
+    if not isinstance(other, Sample):
+      return NotImplemented
+    elif not set(self.spaces).isdisjoint(set(other.spaces)):
+      raise ValueError('Cannot multiply samples with common spaces.')
+    else:
+      return _Mul(self, other)
+
   def __repr__(self) -> str:
     return '{}.{}<{}D, {} elems, {} points>'.format(type(self).__module__, type(self).__qualname__, self.ndims, self.nelems, self.npoints)
 
@@ -123,13 +147,11 @@ class Sample(types.Singleton):
   def index(self) -> Tuple[numpy.ndarray, ...]:
     return tuple(map(self.getindex, range(self.nelems)))
 
-  @abc.abstractmethod
   def getindex(self, __ielem: int) -> numpy.ndarray:
     '''Return the indices of `Sample.points[ielem]` in results of `Sample.eval`.'''
 
     raise NotImplementedError
 
-  @abc.abstractmethod
   def get_evaluable_indices(self, __ielem: evaluable.Array) -> evaluable.Array:
     '''Return the evaluable indices for the given evaluable element index.
 
@@ -263,7 +285,6 @@ class Sample(types.Singleton):
     return function.matmat(self.basis(), array)
 
   @property
-  @abc.abstractmethod
   def tri(self) -> numpy.ndarray:
     '''Triangulation of interior.
 
@@ -271,10 +292,12 @@ class Sample(types.Singleton):
     row defines a simplex by mapping vertices into the list of points.
     '''
 
+    return numpy.concatenate([numpy.take(self.getindex(i), self.get_element_tri(i)) for i in range(self.nelems)], axis=0) if self.nelems else numpy.zeros((0,self.ndims+1), int)
+
+  def get_element_tri(self, __ielem: int) -> numpy.ndarray:
     raise NotImplementedError
 
   @property
-  @abc.abstractmethod
   def hull(self) -> numpy.ndarray:
     '''Triangulation of the exterior hull.
 
@@ -284,6 +307,9 @@ class Sample(types.Singleton):
     triangulations originating from separate elements are disconnected.
     '''
 
+    return numpy.concatenate([numpy.take(self.getindex(i), self.get_element_hull(i)) for i in range(self.nelems)], axis=0) if self.nelems else numpy.zeros((0,self.ndims), int)
+
+  def get_element_hull(self, __ielem: int) -> numpy.ndarray:
     raise NotImplementedError
 
   def subset(self, __mask: numpy.ndarray) -> 'Sample':
@@ -305,7 +331,13 @@ class Sample(types.Singleton):
     subset : :class:`Sample`
     '''
 
-    raise NotImplementedError
+    return self.take_elements(numpy.array([ielem for ielem in range(self.nelems) if __mask[self.getindex(ielem)].any()]))
+
+  def take_elements(self, __indices: numpy.ndarray) -> 'Sample':
+    if len(__indices):
+      return _TakeElements(self, types.arraydata(__indices))
+    else:
+      return Sample.empty(self.spaces, self.ndims)
 
 class _TransformChainsSample(Sample):
 
@@ -336,7 +368,7 @@ class _TransformChainsSample(Sample):
 
   def update_lower_args(self, __ielem: evaluable.Array, points_shape: _PointsShape, transform_chains: _TransformChainsMap, coordinates: _CoordinatesMap) -> Tuple[_PointsShape, _TransformChainsMap, _CoordinatesMap]:
     if self.space in transform_chains or self.space in coordinates:
-      raise ValueError('nested integrals or samples in the same space are not supported')
+      raise ValueError('Nested integrals or samples in the same space are not supported.')
 
     transform_chains = dict(transform_chains)
     transform_chains[self.space] = space_transform_chains = tuple(t.get_evaluable(__ielem) for t in (self.transforms*2)[:2])
@@ -358,6 +390,16 @@ class _TransformChainsSample(Sample):
     transforms = tuple(transform[selection] for transform in self.transforms)
     return Sample.new(self.space, transforms, self.points.take(selection))
 
+  def get_element_tri(self, ielem: int) -> numpy.ndarray:
+    if not 0 <= ielem < self.nelems:
+      raise IndexError('index ouf of range')
+    return self.points.get(ielem).tri
+
+  def get_element_hull(self, ielem: int) -> numpy.ndarray:
+    if not 0 <= ielem < self.nelems:
+      raise IndexError('index ouf of range')
+    return self.points.get(ielem).hull
+
 class _DefaultIndex(_TransformChainsSample):
 
   __slots__ = ()
@@ -368,6 +410,8 @@ class _DefaultIndex(_TransformChainsSample):
     return types.frozenarray(numpy.cumsum([0]+[p.npoints for p in self.points]), copy=False)
 
   def getindex(self, ielem: int) -> numpy.ndarray:
+    if not 0 <= ielem < self.nelems:
+      raise IndexError('index out of range')
     return types.frozenarray(numpy.arange(*self.offsets[ielem:ielem+2]), copy=False)
 
   @property
@@ -406,6 +450,221 @@ class _CustomIndex(_TransformChainsSample):
   @property
   def hull(self) -> numpy.ndarray:
     return numpy.take(self._index, self._parent.hull)
+
+class _Empty(Sample):
+
+  def __init__(self, spaces: Tuple[str, ...], ndims: int) -> None:
+    super().__init__(spaces, ndims, 0, 0)
+
+  def getindex(self, __ielem: int) -> numpy.ndarray:
+    raise IndexError('index out of range')
+
+  def get_evaluable_indices(self, __ielem: evaluable.Array) -> evaluable.Array:
+    return evaluable.Zeros((0,) * len(self.spaces), dtype=int)
+
+  def get_evaluable_weights(self, __ielem: evaluable.Array) -> evaluable.Array:
+    return evaluable.Zeros((0,) * len(self.spaces), dtype=float)
+
+  def update_lower_args(self, __ielem: evaluable.Array, points_shape: _PointsShape, transform_chains: _TransformChainsMap, coordinates: _CoordinatesMap) -> Tuple[_PointsShape, _TransformChainsMap, _CoordinatesMap]:
+    return points_shape, transform_chains, coordinates
+
+  def get_element_tri(self, ielem: int) -> numpy.ndarray:
+    raise IndexError('index out of range')
+
+  def get_element_hull(self, ielem: int) -> numpy.ndarray:
+    raise IndexError('index out of range')
+
+  def take_elements(self, __indices: numpy.ndarray) -> Sample:
+    return self
+
+  def integral(self, __func: function.IntoArray) -> function.Array:
+    func = function.Array.cast(__func)
+    return function.zeros(func.shape, func.dtype)
+
+  def __rmatmul__(self, __func: function.IntoArray) -> function.Array:
+    func = function.Array.cast(__func)
+    return function.zeros((0, *func.shape), func.dtype)
+
+  def basis(self) -> function.Array:
+    return function.zeros((0,), float)
+
+class _Add(Sample):
+
+  def __init__(self, sample1: Sample, sample2: Sample) -> None:
+    assert sample1.spaces == sample2.spaces
+    self._sample1 = sample1
+    self._sample2 = sample2
+    super().__init__(sample1.spaces, sample1.ndims, sample1.nelems + sample2.nelems, sample1.npoints + sample2.npoints)
+
+  def getindex(self, ielem: int) -> numpy.ndarray:
+    if ielem < self._sample1.nelems:
+      return self._sample1.getindex(ielem)
+    else:
+      return self._sample2.getindex(ielem - self._sample1.nelems) + self._sample1.npoints
+
+  def get_element_tri(self, ielem: int) -> numpy.ndarray:
+    if ielem < self._sample1.nelems:
+      return self._sample1.get_element_tri(ielem)
+    else:
+      return self._sample2.get_element_tri(ielem - self._sample1.nelems)
+
+  def get_element_hull(self, ielem: int) -> numpy.ndarray:
+    if ielem < self._sample1.nelems:
+      return self._sample1.get_element_hull(ielem)
+    else:
+      return self._sample2.get_element_hull(ielem - self._sample1.nelems)
+
+  @property
+  def tri(self) -> numpy.ndarray:
+    return numpy.concatenate([self._sample1.tri, self._sample2.tri + self._sample1.npoints])
+
+  @property
+  def hull(self) -> numpy.ndarray:
+    return numpy.concatenate([self._sample1.hull, self._sample2.hull + self._sample1.npoints])
+
+  def take_elements(self, __indices: numpy.ndarray) -> Sample:
+    mask = numpy.less(__indices, self._sample1.nelems)
+    sample1 = self._sample1.take_elements(__indices[mask])
+    sample2 = self._sample2.take_elements(__indices[~mask] - self._sample1.nelems)
+    return sample1 + sample2
+
+  def integral(self, func: function.IntoArray) -> function.Array:
+    return self._sample1.integral(func) + self._sample2.integral(func)
+
+  def __rmatmul__(self, func: function.IntoArray) -> function.Array:
+    return function.concatenate([func @ self._sample1, func @ self._sample2])
+
+class _Mul(Sample):
+
+  def __init__(self, sample1: Sample, sample2: Sample) -> None:
+    assert set(sample1.spaces).isdisjoint(set(sample2.spaces))
+    self._sample1 = sample1
+    self._sample2 = sample2
+    super().__init__(sample1.spaces + sample2.spaces, sample1.ndims + sample2.ndims, sample1.nelems * sample2.nelems, sample1.npoints * sample2.npoints)
+
+  def getindex(self, __ielem: int) -> numpy.ndarray:
+    ielem1, ielem2 = divmod(__ielem, self._sample2.nelems)
+    index1 = self._sample1.getindex(ielem1)
+    index2 = self._sample2.getindex(ielem2)
+    return (index1[:,None] * self._sample2.npoints + index2[None,:]).ravel()
+
+  def get_evaluable_indices(self, __ielem: evaluable.Array) -> evaluable.Array:
+    ielem1, ielem2 = evaluable.divmod(__ielem, self._sample2.nelems)
+    index1 = self._sample1.get_evaluable_indices(ielem1)
+    index2 = self._sample2.get_evaluable_indices(ielem2)
+    return evaluable.appendaxes(index1 * self._sample2.npoints, index2.shape) + evaluable.prependaxes(index2, index1.shape)
+
+  def get_evaluable_weights(self, __ielem: evaluable.Array) -> evaluable.Array:
+    ielem1, ielem2 = evaluable.divmod(__ielem, self._sample2.nelems)
+    weights1 = self._sample1.get_evaluable_weights(ielem1)
+    weights2 = self._sample2.get_evaluable_weights(ielem2)
+    return evaluable.einsum('A,B->AB', weights1, weights2)
+
+  def update_lower_args(self, __ielem: evaluable.Array, points_shape: _PointsShape, transform_chains: _TransformChainsMap, coordinates: _CoordinatesMap) -> Tuple[_PointsShape, _TransformChainsMap, _CoordinatesMap]:
+    ielem1, ielem2 = evaluable.divmod(__ielem, self._sample2.nelems)
+    points_shape, transform_chains, coordinates = self._sample1.update_lower_args(ielem1, points_shape, transform_chains, coordinates)
+    return self._sample2.update_lower_args(ielem2, points_shape, transform_chains, coordinates)
+
+  def get_element_tri(self, ielem: int) -> numpy.ndarray:
+    if self._sample1.ndims == 1:
+      ielem1, ielem2 = divmod(ielem, self._sample2.nelems)
+      npoints2 = len(self._sample2.getindex(ielem2))
+      tri12 = self._sample1.get_element_tri(ielem1)[:,None,:,None] * npoints2 + self._sample2.get_element_tri(ielem2)[None,:,None,:] # ntri1 x ntri2 x 2 x ndims
+      return numeric.overlapping(tri12.reshape(-1, 2*self.ndims), n=self.ndims+1).reshape(-1, self.ndims+1)
+    else:
+      return super().get_element_tri(ielem)
+
+  def get_element_hull(self, ielem: int) -> numpy.ndarray:
+    if self._sample1.ndims == 1:
+      ielem1, ielem2 = divmod(ielem, self._sample2.nelems)
+      npoints2 = len(self._sample2.getindex(ielem2))
+      hull1 = self._sample1.get_element_hull(ielem1)[:,None,:,None] * npoints2 + self._sample2.get_element_tri(ielem2)[None,:,None,:] # 2 x ntri2 x 1 x ndims
+      hull2 = self._sample1.get_element_tri(ielem1)[:,None,:,None] * npoints2 + self._sample2.get_element_hull(ielem2)[None,:,None,:] # ntri1 x nhull2 x 2 x ndims-1
+      return numpy.concatenate([hull1.reshape(-1, self.ndims), numeric.overlapping(hull2.reshape(-1, 2*(self.ndims-1)), n=self.ndims).reshape(-1, self.ndims)])
+    else:
+      return super().get_element_hull(ielem)
+
+  @property
+  def tri(self) -> numpy.ndarray:
+    if self._sample1.ndims == 1:
+      tri12 = self._sample1.tri[:,None,:,None] * self._sample2.npoints + self._sample2.tri[None,:,None,:] # ntri1 x ntri2 x 2 x ndims
+      return numeric.overlapping(tri12.reshape(-1, 2*self.ndims), n=self.ndims+1).reshape(-1, self.ndims+1)
+    else:
+      return super().tri
+
+  @property
+  def hull(self) -> numpy.ndarray:
+    if self._sample1.ndims == 1:
+      hull1 = self._sample1.hull[:,None,:,None] * self._sample2.npoints + self._sample2.tri[None,:,None,:] # 2 x ntri2 x 1 x ndims
+      hull2 = self._sample1.tri[:,None,:,None] * self._sample2.npoints + self._sample2.hull[None,:,None,:] # ntri1 x nhull2 x 2 x ndims-1
+      return numpy.concatenate([hull1.reshape(-1, self.ndims), numeric.overlapping(hull2.reshape(-1, 2*(self.ndims-1)), n=self.ndims).reshape(-1, self.ndims)])
+    else:
+      return super().hull
+
+  def integral(self, func: function.IntoArray) -> function.Array:
+    return self._sample1.integral(self._sample2.integral(func))
+
+  def __rmatmul__(self, func: function.IntoArray) -> function.Array:
+    return function.ravel(func @ self._sample2 @ self._sample1, axis=0)
+
+  def basis(self) -> Sample:
+    basis1 = self._sample1.basis()
+    basis2 = self._sample2.basis()
+    return function.ravel(basis1[:,None] * basis2[None,:], axis=0)
+
+class _TakeElements(Sample):
+
+  __cache__ = '_offsets'
+
+  def __init__(self, parent: Sample, indices: types.arraydata) -> None:
+    assert indices.ndim == 1 and indices.shape[0]
+    self._parent = parent
+    self._indices = numpy.asarray(indices)
+    super().__init__(parent.spaces, parent.ndims, len(self._indices), self._offsets[-1])
+
+  @property
+  def _offsets(self) -> numpy.ndarray:
+    return types.frozenarray(numpy.cumsum([0]+[len(self._parent.getindex(i)) for i in self._indices]))
+
+  def getindex(self, ielem: int) -> numpy.ndarray:
+    if not 0 <= ielem < self.nelems:
+      raise IndexError('index out of range')
+    return numpy.arange(self._offsets[ielem], self._offsets[ielem+1])
+
+  def get_evaluable_indices(self, __ielem: evaluable.Array) -> evaluable.Array:
+    i = evaluable.loop_index('_i', self.nelems)
+    iparent = evaluable.Take(self._indices, i)
+    sizes = evaluable.loop_concatenate(evaluable.InsertAxis(self._parent.get_evaluable_indices(iparent).size, 1), i)
+    offsets = evaluable._SizesToOffsets(sizes)
+
+    iparent = evaluable.Take(self._indices, __ielem)
+    shape = self._parent.get_evaluable_indices(iparent).shape
+    pshape = [shape[-1]]
+    for n in reversed(shape[:-1]):
+      pshape.insert(0, pshape[0] * n)
+    indices = evaluable.Range(pshape[0]) + evaluable.Take(offsets, __ielem)
+    for a, b in zip(shape[:-1], pshape[1:]):
+      indices = evaluable.Unravel(indices, a, b)
+    return indices
+
+  def get_evaluable_weights(self, __ielem: evaluable.Array) -> evaluable.Array:
+    return self._parent.get_evaluable_weights(evaluable.Take(self._indices, __ielem))
+
+  def update_lower_args(self, __ielem: evaluable.Array, points_shape: _PointsShape, transform_chains: _TransformChainsMap, coordinates: _CoordinatesMap) -> Tuple[_PointsShape, _TransformChainsMap, _CoordinatesMap]:
+    return self._parent.update_lower_args(evaluable.Take(self._indices, __ielem), points_shape, transform_chains, coordinates)
+
+  def get_element_tri(self, __ielem: int) -> numpy.ndarray:
+    if not 0 <= __ielem < self.nelems:
+      raise IndexError('index ouf of range')
+    return self._parent.get_element_tri(numpy.take(self._indices, __ielem))
+
+  def get_element_hull(self, __ielem: int) -> numpy.ndarray:
+    if not 0 <= __ielem < self.nelems:
+      raise IndexError('index ouf of range')
+    return self._parent.get_element_hull(numpy.take(self._indices, __ielem))
+
+  def take_elements(self, __indices: numpy.ndarray) -> Sample:
+    return self._parent.take_elements(numpy.take(self._indices, __indices))
 
 def eval_integrals(*integrals: evaluable.AsEvaluableArray, **arguments: Mapping[str, numpy.ndarray]) -> Tuple[Union[numpy.ndarray, matrix.Matrix], ...]:
   '''Evaluate integrals.
