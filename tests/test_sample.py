evalf/nutils

commit a2023666590d3213654ec4f2e4ce927d8dc1e324

add evaluable.eval_sparse
This patch adds evaluable.eval_sparse and deprecates
sample.eval_integrals_sparse in favour of the new function.

nutils/sample.py
@@ -35,7 +35,7 @@ selected sample points, and is typically used in combination with the "bezier"
 set.
 '''
 
-from . import types, points, util, function, evaluable, parallel, numeric, matrix, sparse
+from . import types, points, util, function, evaluable, parallel, numeric, matrix, sparse, warnings
 from .pointsseq import PointsSequence
 from .transformseq import Transforms
 from .transform import EvaluableTransformChain
@@ -45,8 +45,6 @@ import numpy, numbers, collections.abc, os, treelog as log, abc
 _PointsShape = Tuple[evaluable.Array, ...]
 _TransformChainsMap = Mapping[str, Tuple[EvaluableTransformChain, EvaluableTransformChain]]
 _CoordinatesMap = Mapping[str, evaluable.Array]
-
-graphviz = os.environ.get('NUTILS_GRAPHVIZ')
 
 def argdict(arguments) -> Mapping[str, numpy.ndarray]:
   if len(arguments) == 1 and 'arguments' in arguments and isinstance(arguments['arguments'], collections.abc.Mapping):
@@ -207,7 +205,7 @@ class Sample(types.Singleton):
         Optional arguments for function evaluation.
     '''
 
-    return eval_integrals_sparse(*map(self.integral, funcs), **(arguments or {}))
+    return evaluable.eval_sparse(map(self.integral, funcs), **(arguments or {}))
 
   def integral(self, __func: function.IntoArray) -> function.Array:
     '''Create Integral object for postponed integration.
@@ -250,7 +248,7 @@ class Sample(types.Singleton):
         Optional arguments for function evaluation.
     '''
 
-    return eval_integrals_sparse(*map(self, funcs), **(arguments or {}))
+    return evaluable.eval_sparse(map(self, funcs), **(arguments or {}))
 
   def __call__(self, __func: function.IntoArray) -> function.Array:
     func = _ConcatenatePoints(function.Array.cast(__func), self)
@@ -732,11 +730,15 @@ def eval_integrals(*integrals: evaluable.AsEvaluableArray, **arguments: Mapping[
   results : :class:`tuple` of arrays and/or :class:`nutils.matrix.Matrix` objects.
   '''
 
-  with log.iter.fraction('assembling', eval_integrals_sparse(*integrals, **argdict(arguments))) as retvals:
+  with log.iter.fraction('assembling', evaluable.eval_sparse(integrals, **argdict(arguments))) as retvals:
     return tuple(_convert(retval, inplace=True) for retval in retvals)
 
 def eval_integrals_sparse(*integrals: evaluable.AsEvaluableArray, **arguments: Mapping[str, numpy.ndarray]) -> Tuple[numpy.ndarray, ...]:
-  '''Evaluate integrals into sparse data.
+  '''
+  .. deprecated:: 7.0
+      sample.eval_integrals_sparse is deprecated, use function.eval_sparse instead
+
+  Evaluate integrals into sparse data.
 
   Evaluate one or several postponed integrals. By evaluating them
   simultaneously, rather than using :meth:`nutils.function.Array.eval` on each
@@ -755,9 +757,8 @@ def eval_integrals_sparse(*integrals: evaluable.AsEvaluableArray, **arguments: M
   results : :class:`tuple` of arrays and/or :class:`nutils.matrix.Matrix` objects.
   '''
 
-  integrals = tuple(integral.as_evaluable_array.assparse for integral in integrals)
-  with evaluable.Tuple(tuple(integrals)).optimized_for_numpy.session(graphviz=graphviz) as eval:
-    return eval(**arguments)
+  warnings.deprecation('sample.eval_integrals_sparse is deprecated, use function.eval_sparse instead')
+  return function.eval_sparse(integrals, **arguments)
 
 def _convert(data: numpy.ndarray, inplace: bool = False) -> Union[numpy.ndarray, matrix.Matrix]:
   '''Convert a two-dimensional sparse object to an appropriate object.

nutils/evaluable.py
@@ -50,7 +50,9 @@ else:
 
 from . import debug_flags, util, types, numeric, cache, warnings, parallel, sparse
 from ._graph import Node, RegularNode, DuplicatedLeafNode, InvisibleNode, Subgraph
-import numpy, sys, itertools, functools, operator, inspect, numbers, builtins, re, types as builtin_types, abc, collections.abc, math, treelog as log, weakref, time, contextlib, subprocess
+import numpy, sys, itertools, functools, operator, inspect, numbers, builtins, re, types as builtin_types, abc, collections.abc, math, treelog as log, weakref, time, contextlib, subprocess, os
+
+graphviz = os.environ.get('NUTILS_GRAPHVIZ')
 
 isevaluable = lambda arg: isinstance(arg, Evaluable)
 
@@ -4361,6 +4363,27 @@ def einsum(fmt, *args, **dims):
   for i in range(len(sout), len(sall)):
     ret = Sum(ret)
   return ret
+
+@util.single_or_multiple
+def eval_sparse(funcs: AsEvaluableArray, **arguments: typing.Mapping[str, numpy.ndarray]) -> typing.Tuple[numpy.ndarray, ...]:
+  '''Evaluate one or several Array objects as sparse data.
+
+  Args
+  ----
+  funcs : :class:`tuple` of Array objects
+      Arrays to be evaluated.
+  arguments : :class:`dict` (default: None)
+      Optional arguments for function evaluation.
+
+  Returns
+  -------
+  results : :class:`tuple` of sparse data arrays
+  '''
+
+  funcs = tuple(func.as_evaluable_array.assparse for func in funcs)
+  with Tuple(funcs).optimized_for_numpy.session(graphviz=graphviz) as eval:
+    return eval(**arguments)
+
 
 if __name__ == '__main__':
   # Diagnostics for the development for simplify operations.

nutils/solver.py
@@ -50,7 +50,7 @@ In addition to ``solve_linear`` the solver module defines ``newton`` and
 time dependent problems.
 """
 
-from . import function, evaluable, cache, numeric, sample, types, util, matrix, warnings, sparse
+from . import function, evaluable, cache, numeric, types, util, matrix, warnings, sparse
 import abc, numpy, itertools, functools, numbers, collections, math, inspect, treelog as log
 
 
@@ -945,7 +945,7 @@ def _integrate_blocks(*blocks, arguments, mask):
   *scalars, residuals, jacobians = blocks
   assert len(residuals) == len(mask)
   assert len(jacobians) == len(mask)**2
-  data = iter(sample.eval_integrals_sparse(*scalars, *residuals, *jacobians, **arguments))
+  data = iter(evaluable.eval_sparse((*scalars, *residuals, *jacobians), **arguments))
   nrg = [sparse.toarray(next(data)) for _ in range(len(scalars))]
   res = [sparse.take(next(data), [m]) for m in mask]
   jac = [[sparse.take(next(data), [mi, mj]) for mj in mask] for mi in mask]

tests/test_solver.py
@@ -1,4 +1,4 @@
-from nutils import solver, mesh, function, cache, types, numeric, warnings, sample, sparse
+from nutils import solver, mesh, function, cache, types, numeric, warnings, evaluable, sparse
 from nutils.testing import *
 import numpy, contextlib, tempfile, itertools, logging
 
@@ -86,7 +86,7 @@ class navierstokes(TestCase):
   def assert_resnorm(self, lhs):
     res = self.residual.eval(arguments=dict(dofs=lhs))[numpy.isnan(self.cons)] if self.single \
       else numpy.concatenate([sparse.toarray(sparse.take(r, [numpy.isnan(self.cons[d])]) if d in self.cons else r)
-        for d, r in zip(self.dofs, sample.eval_integrals_sparse(*self.residual, **lhs))])
+        for d, r in zip(self.dofs, evaluable.eval_sparse(self.residual, **lhs))])
     resnorm = numpy.linalg.norm(res)
     self.assertLess(resnorm, self.tol)
 
@@ -142,7 +142,7 @@ class finitestrain(TestCase):
     self.tol = 1e-10
 
   def assert_resnorm(self, lhs):
-    res, = sample.eval_integrals_sparse(self.residual, dofs=lhs)
+    res = evaluable.eval_sparse(self.residual, dofs=lhs)
     resnorm = numpy.linalg.norm(sparse.toarray(res)[~self.boolcons])
     self.assertLess(resnorm, self.tol)
 

commit 92cd3d6c40fd63740d6f722185c7a7cc60c93df1

fix sample.eval_integrals_sparse
This patch fixes a bug that was introduced in commit a202366 of #653, which
introduced evaluable.eval_sparse and deprecated sample.eval_integrals_sparse.
In doing so, the method was erroneously changed to dispatch to
function.eval_sparse, a location that had been temporarily considered but was
ultimately rejected. The bug was not noticed because all unit tests were
updated to use the new method directly; two tests are added to correct this.

nutils/sample.py
@@ -757,8 +757,8 @@ def eval_integrals_sparse(*integrals: evaluable.AsEvaluableArray, **arguments: M
   results : :class:`tuple` of arrays and/or :class:`nutils.matrix.Matrix` objects.
   '''
 
-  warnings.deprecation('sample.eval_integrals_sparse is deprecated, use function.eval_sparse instead')
-  return function.eval_sparse(integrals, **arguments)
+  warnings.deprecation('sample.eval_integrals_sparse is deprecated, use evaluable.eval_sparse instead')
+  return evaluable.eval_sparse(integrals, **argdict(arguments))
 
 def _convert(data: numpy.ndarray, inplace: bool = False) -> Union[numpy.ndarray, matrix.Matrix]:
   '''Convert a two-dimensional sparse object to an appropriate object.

tests/test_sample.py
@@ -409,6 +409,15 @@ class integral(TestCase):
       self.topo.integral('basis_n d:x' @ self.ns, degree=2).eval(),
       places=15)
 
+  def test_eval_integrals(self):
+    v, = sample.eval_integrals(self.topo.integral('basis_n d:x' @ self.ns, degree=2))
+    self.assertAllAlmostEqual(self.topo.integrate('basis_n d:x' @ self.ns, degree=2), v, places=15)
+
+  def test_eval_integrals_sparse(self):
+    with self.assertWarns(warnings.NutilsDeprecationWarning):
+      data, = sample.eval_integrals_sparse(self.topo.integral('basis_n d:x' @ self.ns, degree=2))
+    self.assertAllAlmostEqual(self.topo.integrate('basis_n d:x' @ self.ns, degree=2), sparse.toarray(data), places=15)
+
   def test_args(self):
     self.assertAlmostEqual(
       self.topo.integrate('v d:x' @ self.ns, degree=2, arguments=dict(lhs=self.lhs)),