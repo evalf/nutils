a530588dd519f507e1bc19e3474ba3634af87739
Joost van Zwieten <joostvanzwieten@evalf.com>
2021-12-15 21:21:30 +0100

    prevent inflation warnings in selected tests
    This patch calls `simpiflied` on two evaluable arrays in unittests to prevent
    explicit inflation warnings (turned into errors by default) arising from future
    changes to the function tree.

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -616,7 +616,7 @@ class TopologyAssertions:
     imask = numpy.zeros(len(interfaces), dtype=int)
     coordinates = evaluable.Points(evaluable.NPoints(), boundary.ndims)
     transform_chain = transform.EvaluableTransformChain.from_argument('trans', domain.transforms.todims, boundary.ndims)
-    lowered_geom = geom.lower(coordinates.shape[:-1], {domain.space: (transform_chain,)*2}, {domain.space: coordinates})
+    lowered_geom = geom.lower(coordinates.shape[:-1], {domain.space: (transform_chain,)*2}, {domain.space: coordinates}).simplified
     for ielem, ioppelems in enumerate(domain.connectivity):
       for iedge, ioppelem in enumerate(ioppelems):
         etrans, eref = domain.references[ielem].edges[iedge]
