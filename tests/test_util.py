5642d174d5678b40e26e9451c99401dfa392c91f
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2022-09-05 00:01:41 +0200

    remove unused utilities
    This patch removes the following utilities that are no longer in use:
    
    - regularize
    - tri_merge
    - tri_interpolator
    - linear_regressor

--- tests/test_util.py
+++ tests/test_util.py
@@ -10,73 +10,7 @@ import treelog
 import datetime
 import numpy
 import sys
-
-
-@parametrize
-class tri(TestCase):
-
-    # Triangles and node numbering:
-    #
-    #   2/4-(5)
-    #    | \ |
-    #   (0)-1/3
-
-    def setUp(self):
-        super().setUp()
-        self.x = numpy.array([[0, 0], [1, 0], [0, 1], [1, 0], [0, 1], [1, 1]], dtype=float)
-        self.tri = numpy.array([[0, 1, 2], [3, 4, 5]])
-
-    @requires('scipy')
-    def test_merge(self):
-        tri_merged = util.tri_merge(self.tri, self.x, mergetol=self.mergetol).tolist()
-        tri_expected = self.tri.tolist() if self.mergetol < 0 else [[0, 1, 2], [1, 2, 5]] if self.mergetol < 1 else [[0, 0, 0], [0, 0, 0]]
-        self.assertEqual(tri_merged, tri_expected)
-
-    @requires('matplotlib', 'scipy')
-    def test_interpolate(self):
-        interpolate = util.tri_interpolator(self.tri, self.x, mergetol=self.mergetol)
-        x = [.1, .9],
-        if self.mergetol < 0:
-            with self.assertRaises(RuntimeError):
-                interpolate[x]
-        else:
-            f = interpolate[x]
-            vtri = [0, 0], [1, 0], [0, 1], [10, 10], [10, 10], [1, 1]
-            vx = f(vtri)  # note: v[3] and v[4] should be ignored, leaving a linear ramp
-            self.assertEqual(vx.shape, (1, 2))
-            if self.mergetol < 1:
-                self.assertEqual(vx.tolist(), list(x))
-            else:
-                self.assertTrue(numpy.isnan(vx).all())
-
-    @parametrize.enable_if(lambda mergetol: 0 <= mergetol < 1)
-    @requires('matplotlib', 'scipy')
-    def test_outofbounds(self):
-        interpolate = util.tri_interpolator(self.tri, self.x, mergetol=self.mergetol)
-        x = [.5, .5], [1.5, .5]
-        vtri = 0, 1, 0, 10, 10, 1
-        vx = interpolate[x](vtri)
-        self.assertEqual(vx.shape, (2,))
-        self.assertEqual(vx[0], .5)
-        self.assertTrue(numpy.isnan(vx[1]))
-
-
-tri(mergetol=-1)
-tri(mergetol=0)
-tri(mergetol=.1)
-tri(mergetol=2)
-
-
-class linreg(TestCase):
-
-    def test_linear(self):
-        a = numpy.array([[0, 1], [-1, 0]])
-        b = numpy.array([[0, 1], [0, 1]])
-        linreg = util.linear_regressor()
-        ab0, ab1, ab2 = [linreg.add(x, a * x + b) for x in range(3)]
-        self.assertTrue(numpy.isnan(ab0).all())
-        self.assertEqual([a.tolist(), b.tolist()], ab1.tolist())
-        self.assertEqual([a.tolist(), b.tolist()], ab2.tolist())
+import contextlib
 
 
 class pairwise(TestCase):
