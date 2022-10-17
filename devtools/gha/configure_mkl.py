7ba3ef30d6261352211b39896e1ad0c4eb4bd103
Joost van Zwieten <joostvanzwieten@evalf.com>
2022-10-17 13:35:00 +0200

    disable symlinking libmkl in GitHub actions
    In a previous patch Nutils gained the ability to load versioned libraries of
    MKL, e.g. `libmkl.so.2` on Linux, in addition to unversioned libraries, e.g.
    `libmkl.so`. This patch removes the symlinking to `libmkl.so` in case only a
    versioned library was found in GitHub Actions

--- devtools/gha/configure_mkl.py
+++ devtools/gha/configure_mkl.py
@@ -16,10 +16,6 @@ else:
     log.error('cannot find any of {}'.format(' '.join(candidates)))
     raise SystemExit(1)
 
-lib = os.path.splitext(path)[0]
-if not os.path.exists(lib):
-    os.symlink(path, lib)
-
 ld_library_path = os.pathsep.join(filter(None, (os.environ.get('LD_LIBRARY_PATH', ''), os.path.dirname(path))))
 with open(os.environ['GITHUB_ENV'], 'a') as f:
     print('LD_LIBRARY_PATH={}'.format(ld_library_path), file=f)
