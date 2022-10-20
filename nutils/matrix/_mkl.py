0759d4736812ff74ffc87606855a58acfa4b16d0
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2022-10-20 15:51:44 +0200

    use SPD solver for diagonally dominant matrices
    This patch adds a test for diagonal dominance and switches Pardiso to the SPD
    matrix type to benefit from this property.

--- nutils/matrix/_mkl.py
+++ nutils/matrix/_mkl.py
@@ -264,10 +264,17 @@ class MKLMatrix(Matrix):
         upper = numpy.zeros(len(self.data), dtype=bool)
         rowptr = numpy.empty_like(self.rowptr)
         rowptr[0] = 1
+        diagdom = True
         for irow, (n, m) in enumerate(numeric.overlapping(self.rowptr-1), start=1):
             d = n + self.colidx[n:m].searchsorted(irow)
             upper[d:m] = True
             rowptr[irow] = rowptr[irow-1] + (m-d)
-        return Pardiso(mtype=dict(f=-2, c=6)[self.dtype.kind], a=self.data[upper], ia=rowptr, ja=self.colidx[upper], **args)
+            diagdom = diagdom and d < m and self.colidx[d] == irow and abs(self.data[n:m]).sum() < 2 * abs(self.data[d])
+        if diagdom:
+            log.debug('matrix is diagonally dominant, solving as SPD')
+            mtype = dict(f=2, c=4)
+        else:
+            mtype = dict(f=-2, c=6)
+        return Pardiso(mtype=mtype[self.dtype.kind], a=self.data[upper], ia=rowptr, ja=self.colidx[upper], **args)
 
 # vim:sw=2:sts=2:et
