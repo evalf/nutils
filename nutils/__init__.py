708b0e2b27f77c8a90d612ca85b648919327fbde
Joost van Zwieten <joostvanzwieten@evalf.com>
2022-11-30 09:52:31 +0100

    add nutils.__version__ alias
    In a follow-up patch `setup.py` will be replaced by a `pyproject.toml` using
    Flit as build system. To prepare for this change, this patch adds
    `nutils.__version__` as an alias for `nutils.version`, which is used by Flit to
    determine the version of the package.

--- nutils/__init__.py
+++ nutils/__init__.py
@@ -7,7 +7,7 @@ from distutils.version import LooseVersion
 assert sys.version_info >= (3, 5)
 assert LooseVersion(numpy.version.version) >= LooseVersion('1.16'), 'nutils requires numpy 1.16 or higher, got {}'.format(numpy.version.version)
 
-version = '8.0a0'
+__version__ = version = '8.0a0'
 version_name = None
 long_version = ('{} "{}"' if version_name else '{}').format(version, version_name)
 
