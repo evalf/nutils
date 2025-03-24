740ab80ee93c9ab521688a36310601c73fa20de6
Joost van Zwieten <joostvanzwieten@evalf.com>
2025-03-24 13:32:09 +0100

    Implement Polyval._compile_expression

--- nutils/evaluable.py
+++ nutils/evaluable.py
@@ -4328,7 +4328,8 @@ class Polyval(Array):
     def shape(self):
         return self.points.shape[:-1] + self.coeffs.shape[:-1]
 
-    evalf = staticmethod(poly.eval_outer)
+    def _compile_expression(self, py_self, points, coeffs):
+        return _pyast.Variable('poly').get_attr('eval_outer').call(points, coeffs)
 
     def _derivative(self, var, seen):
         if self.dtype == complex:
