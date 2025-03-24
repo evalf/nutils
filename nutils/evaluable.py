2df73a6b07c6101989a60c8853f3b3f797a5fc5c
Joost van Zwieten <joostvanzwieten@evalf.com>
2025-03-24 13:27:11 +0100

    Implement LogicalNot._compile_expression

--- nutils/evaluable.py
+++ nutils/evaluable.py
@@ -2666,7 +2666,8 @@ class LogicalNot(Pointwise):
     def dependencies(self):
         return self.x,
 
-    evalf = staticmethod(numpy.logical_not)
+    def _compile_expression(self, py_self, add_constant, x):
+        return _pyast.Variable('numpy').get_attr('logical_not').call(x)
 
     @cached_property
     def dtype(self):
